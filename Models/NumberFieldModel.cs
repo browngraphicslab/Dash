1443e44fba3af30f6324941574e2b4beddcbc4c5
kyubin-kwon <kyu_bin_kwon@brown.edu>
2017-06-12 17:50:34 -0400

    new classes added

---
+++ Models/NumberFieldModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Dash
+{
+    class NumberFieldModel : FieldModel
+    {
+        public NumberFieldModel() { }
+
+        public NumberFieldModel(double data)
+        {
+            Data = data;
+        }
+
+        public double Data { get; set; }
+
+        /// <summary>
+        /// Creates TextBlock using layout information from template and Data 
+        /// </summary>
+        public override UIElement MakeView(TemplateModel template)
+        {
+            TextTemplateModel textTemplate = template as TextTemplateModel;
+            Debug.Assert(textTemplate != null);
+            //    TextViewModel vm = new TextViewModel(this, template);
+            TextBlock tb = new TextBlock
+            {
+                Text = Data.ToString(CultureInfo.InvariantCulture)
+            };
+            Canvas.SetTop(tb, textTemplate.Top);
+            Canvas.SetLeft(tb, textTemplate.Left);
+            tb.FontWeight = textTemplate.FontWeight;
+            tb.TextWrapping = textTemplate.TextWrapping;
+            tb.Visibility = textTemplate.Visibility;
+            return tb;
+        }
+    }
+}
---
+++ Models/OperatorModels/OperatorFieldModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Dash
+{
+    public abstract class OperatorFieldModel : FieldModel
+    {
+        /// <summary>
+        /// Dictionary that maps Field input name to the ReferenceFieldModel that it is set to
+        /// </summary>
+        protected Dictionary<string, ReferenceFieldModel> InputReferences { get; set; } = new Dictionary<string, ReferenceFieldModel>();
+
+        public void AddInputReference(string fieldName, ReferenceFieldModel reference)
+        {
+            // remove the output reference of previous input reference document FieldModel 
+            //if (InputReferences.ContainsKey(fieldName))
+            //{
+            //    FieldModel fm = DocumentManager.GetFieldFromDocument(InputReferences[fieldName]);
+            //    fm.RemoveOutputReference(this);
+            //}
+            InputReferences[fieldName] = reference;
+            //DocumentModel doc = DocumentManager.GetDocumentWithID(reference.DocId);
+            //doc.Fields[reference.FieldKey].Updated += Updated;
+        }
+
+        public override UIElement MakeView(TemplateModel template)
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract Dictionary<String, FieldModel> Execute(/*Dictionary<String, FieldModel> args*/);
+    }
+}
