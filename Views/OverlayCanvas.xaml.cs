51c82a14319f3d0ba43dde249fc040b8fa5a72d6
Anna White <anna_white@brown.edu>
2017-06-22 13:21:46 -0400

    commenting some models

--- Views/OverlayCanvas.xaml.cs
+++ Views/OverlayCanvas.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Shapes;
 using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -33,6 +34,36 @@ namespace Dash
 
             Debug.Assert(Instance == null);
             Instance = this;
+
+            // test server to API
+
+            // set uop server XAML object
+            ServerXAML.UIElement test = new ServerXAML.UIElement();
+            test.Height = 100;
+            test.Width = 100;
+            test.Visibility = ServerXAML.Visibility.Visible;
+
+            // initialize mapping
+            Mapper.Initialize(cfg => {
+                cfg.CreateMap<ServerXAML.UIElement, FrameworkElement>();
+            });
+
+            // create obj to be mapped to 
+            FrameworkElement myMan = new TextBox();
+            Debug.WriteLine(test.Height == myMan.Height);
+
+            // map test into myMan
+            Mapper.Map(test, myMan);
+
+            // just add visual stuff s.t. we can see the result
+            TextBox g = (TextBox)myMan;
+            g.Text = "IT WORKS!";
+            g.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            g.Margin = new Thickness(0, 0, 0, 0);
+
+            // the server XAML maps the 100 width and height and visibility
+            // into the new element!
+            XContainer.Children.Add(g);
         }
 
         private void AddDocumentsTapped(object sender, TappedRoutedEventArgs e)
