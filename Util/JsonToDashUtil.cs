d16248fe1283a51d2bad44be2f79ad2ed344d75c
Luke Murray <luke_murray@brown.edu>
2017-06-27 08:01:46 -0400

    Removed null reference bug from the json parsing

--- Util/JsonToDashUtil.cs
+++ Util/JsonToDashUtil.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage;
+using DashShared;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -38,7 +39,6 @@ namespace Dash
 
         public static void ParseJson(JToken jToken)
         {
-            // parse the json into whatever it is
 
             // deal with object
             if (jToken.Type == JTokenType.Object)
@@ -51,7 +51,7 @@ namespace Dash
             }
 
             // deal with array
-            if (jToken.Type == JTokenType.Array)
+            else if (jToken.Type == JTokenType.Array)
             {
                 var myArray = jToken as JArray;
                 foreach (var item in myArray)
@@ -60,60 +60,62 @@ namespace Dash
                 }
             }
 
-            // TODO IF ELSE OR RETURN OTHERWISE NULL EXCEPTION BELOW
-
             // deal with value
-            try
+            else
             {
-                var myValue = jToken as JValue;
-                var type = myValue.Type;
-                switch (type)
+                try
                 {
-                    case JTokenType.None:
-                        break;
-                    case JTokenType.Object:
-                        break;
-                    case JTokenType.Array:
-                        break;
-                    case JTokenType.Constructor:
-                        break;
-                    case JTokenType.Property:
-                        break;
-                    case JTokenType.Comment:
-                        break;
-                    case JTokenType.Integer:
-                        break;
-                    case JTokenType.Float:
-                        break;
-                    case JTokenType.String:
-                        break;
-                    case JTokenType.Boolean:
-                        break;
-                    case JTokenType.Null:
-                        break;
-                    case JTokenType.Undefined:
-                        break;
-                    case JTokenType.Date:
-                        break;
-                    case JTokenType.Raw:
-                        break;
-                    case JTokenType.Bytes:
-                        break;
-                    case JTokenType.Guid:
-                        break;
-                    case JTokenType.Uri:
-                        break;
-                    case JTokenType.TimeSpan:
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
+                    var myValue = jToken as JValue;
+                    var type = myValue.Type;
+                    switch (type)
+                    {
+                        case JTokenType.None:
+                            break;
+                        case JTokenType.Object:
+                            break;
+                        case JTokenType.Array:
+                            break;
+                        case JTokenType.Constructor:
+                            break;
+                        case JTokenType.Property:
+                            break;
+                        case JTokenType.Comment:
+                            break;
+                        case JTokenType.Integer:
+                            break;
+                        case JTokenType.Float:
+                            break;
+                        case JTokenType.String:
+                            break;
+                        case JTokenType.Boolean:
+                            break;
+                        case JTokenType.Null:
+                            break;
+                        case JTokenType.Undefined:
+                            break;
+                        case JTokenType.Date:
+                            break;
+                        case JTokenType.Raw:
+                            break;
+                        case JTokenType.Bytes:
+                            break;
+                        case JTokenType.Guid:
+                            break;
+                        case JTokenType.Uri:
+                            break;
+                        case JTokenType.TimeSpan:
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+                catch (InvalidCastException e)
+                {
+                    Console.WriteLine(e);
+                    throw;
                 }
             }
-            catch (InvalidCastException e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+            
         }
 
         // recursively yield all children of json
