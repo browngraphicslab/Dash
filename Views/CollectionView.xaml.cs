f7b35d925e03f1260c300f57baa4fa875a76cb3b
WINDOWS-TG53K0C\Administrator <david_wang1@brown.edu>
2017-07-11 16:56:50 -0400

    removed duplicate code

--- Views/CollectionView.xaml.cs
+++ Views/CollectionView.xaml.cs
@@ -148,6 +148,7 @@ namespace Dash
             {
                 OpenMenu();
                 SetEnabled(true);
+                xOuterGrid.BorderThickness = new Thickness(0);
             }
         }
 
@@ -553,7 +554,6 @@ namespace Dash
         private Path _connectionLine;
 
         private MultiBinding<PathFigureCollection> _lineBinding;
-        private BezierConverter _converter;
 
         /// <summary>
         /// IOReference (containing reference to fields) being referred to when creating the visual connection between fields 
@@ -567,83 +567,7 @@ namespace Dash
         /// </summary>
         private HashSet<uint> _currentPointers = new HashSet<uint>();
 
-        /// <summary>
-        /// Dictionary that maps DocumentViews on maincanvas to its DocumentID 
-        /// </summary>
-        //private Dictionary<string, DocumentView> _documentViews = new Dictionary<string, DocumentView>();
-
-        private class BezierConverter : IValueConverter
-        {
-            public BezierConverter(FrameworkElement element1, FrameworkElement element2, FrameworkElement toElement)
-            {
-                Element1 = element1;
-                Element2 = element2;
-                ToElement = toElement;
-                _figure = new PathFigure();
-                _bezier = new BezierSegment();
-                _figure.Segments.Add(_bezier);
-                _col.Add(_figure);
-            }
-
-            public FrameworkElement Element1 { get; set; }
-            public FrameworkElement Element2 { get; set; }
-
-            public FrameworkElement ToElement { get; set; }
-
-            public Point Pos2 { get; set; }
-
-            private PathFigureCollection _col = new PathFigureCollection();
-            private PathFigure _figure;
-            private BezierSegment _bezier;
-
-            public object Convert(object value, Type targetType, object parameter, string language)
-            {
-                var pos1 = Element1.TransformToVisual(ToElement)
-                    .TransformPoint(new Point(Element1.ActualWidth / 2, Element1.ActualHeight / 2));
-                var pos2 = Element2?.TransformToVisual(ToElement)
-                               .TransformPoint(new Point(Element2.ActualWidth / 2, Element2.ActualHeight / 2)) ?? Pos2;
-
-                double offset = Math.Abs((pos1.X - pos2.X) / 3);
-                if (pos1.X < pos2.X)
-                {
-                    _figure.StartPoint = new Point(pos1.X + Element1.ActualWidth / 2, pos1.Y);
-                    _bezier.Point1 = new Point(pos1.X + offset, pos1.Y);
-                    _bezier.Point2 = new Point(pos2.X - offset, pos2.Y);
-                    _bezier.Point3 = new Point(pos2.X - (Element2?.ActualWidth / 2 ?? 0), pos2.Y);
-                }
-                else
-                {
-                    _figure.StartPoint = new Point(pos1.X - Element1.ActualWidth / 2, pos1.Y);
-                    _bezier.Point1 = new Point(pos1.X - offset, pos1.Y);
-                    _bezier.Point2 = new Point(pos2.X + offset, pos2.Y);
-                    _bezier.Point3 = new Point(pos2.X + (Element2?.ActualWidth / 2 ?? 0), pos2.Y);
-                }
-
-                return _col;
-            }
-            
-
-            public object ConvertBack(object value, Type targetType, object parameter, string language)
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        private class VisibilityConverter : IValueConverter
-        {
-            public object Convert(object value, Type targetType, object parameter, string language)
-            {
-                bool isEditorMode = (bool)value;
-                return isEditorMode ? Visibility.Visible : Visibility.Collapsed;
-            }
-
-            public object ConvertBack(object value, Type targetType, object parameter, string language)
-            {
-                throw new NotImplementedException();
-            }
-        }
         #endregion
-
         
         private void ConnectionEllipse_OnManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
         {
