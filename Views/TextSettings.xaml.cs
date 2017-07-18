80648986f50d6c73a185eff5faa8754ed7c615f5
Crystal Chen <hui_chen@brown.edu>
2017-07-18 17:25:37 -0400

    removed abstracted settings code

--- Views/TextSettings.xaml.cs
+++ Views/TextSettings.xaml.cs
@@ -30,53 +30,9 @@ namespace Dash
         {
             this.editedLayoutDocument = editedLayoutDocument;
 
-            //Border LeftButton = new Border()
-            //{
-            //    Child = new Image()
-            //    {
-            //        Source =
-            //            new BitmapImage(new Uri(@"\Assets\Leftlg.png"))
-            //    },
-            //    //Child = new TextBlock() { Text = "Left" },
-            //    Width = 30,
-            //    Height = 20
-            //};
-            //Border CenterButton = new Border()
-            //{
-            //    Child = new Image()
-            //    {
-            //        Source =
-            //            new BitmapImage(new Uri(@"\Assets\Centerlg.png"))
-            //    },
-            //    //Child = new TextBlock() { Text = "Center" },
-            //    Width = 30,
-            //    Height = 20
-            //};
-            //Border RightButton = new Border()
-            //{
-            //    Child = new Image()
-            //    {
-            //        Source =
-            //            new BitmapImage(new Uri(@"\Assets\Rightlg.png"))
-            //    },
-            //    //Child = new TextBlock() { Text = "Right" },
-            //    Width = 30,
-            //    Height = 20
-            //};
-
-            //xAlignmentListView.ItemsSource = new List<UIElement>
-            //{
-            //    LeftButton,
-            //    CenterButton,
-            //    RightButton
-            //};
-
             var docContextList = context.DocContextList; 
             xSizeRow.Children.Add(new SizeSettings(editedLayoutDocument,docContextList));
             xPositionRow.Children.Add(new PositionSettings(editedLayoutDocument,docContextList));
-            //BindWidth(editedLayoutDocument, docContextList);
-            //BindHeight(editedLayoutDocument, docContextList);
-            //BindPosition(editedLayoutDocument, docContextList);
             BindFontWeight(editedLayoutDocument, context);
             BindFontSize(editedLayoutDocument, context);
             BindFontAlignment(editedLayoutDocument, context);
@@ -141,65 +97,5 @@ namespace Dash
             xFontSizeSlider.SetBinding(Slider.ValueProperty, fontSizeBinding);
             xFontSizeTextBox.SetBinding(TextBox.TextProperty, fontSizeBinding);
         }
-
-        //private void BindPosition(DocumentController docController, IEnumerable<DocumentController> docContextList)
-        //{
-        //    var positionController = docController.GetDereferencedField(DashConstants.KeyStore.PositionFieldKey, docContextList) as PointFieldModelController;
-        //    Debug.Assert(positionController != null);
-
-        //    var converter = new StringCoordinateToPointConverter(positionController.Data);
-
-        //    var xPositionBinding = new Binding
-        //    {
-        //        Source = positionController,
-        //        Path = new PropertyPath(nameof(positionController.Data)),
-        //        Mode = BindingMode.TwoWay,
-        //        Converter = converter,
-        //        ConverterParameter = Coordinate.X,
-        //        UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
-        //    };
-        //    xHorizontalPositionTextBox.SetBinding(TextBox.TextProperty, xPositionBinding);
-
-        //    var yPositionBinding = new Binding
-        //    {
-        //        Source = positionController,
-        //        Path = new PropertyPath(nameof(positionController.Data)),
-        //        Mode = BindingMode.TwoWay,
-        //        Converter = converter,
-        //        ConverterParameter = Coordinate.Y,
-        //        UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
-        //    };
-        //    xVerticalPositionTextBox.SetBinding(TextBox.TextProperty, yPositionBinding);
-        //}
-
-        //private void BindHeight(DocumentController docController, IEnumerable<DocumentController> docContextList)
-        //{
-        //    var heightController = docController.GetDereferencedField(DashConstants.KeyStore.HeightFieldKey, docContextList) as NumberFieldModelController;
-        //    Debug.Assert(heightController != null);
-
-        //    var heightBinding = new Binding
-        //    {
-        //        Source = heightController,
-        //        Path = new PropertyPath(nameof(heightController.Data)),
-        //        Mode = BindingMode.TwoWay,
-        //        UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
-        //    };
-        //    xHeightTextBox.SetBinding(TextBox.TextProperty, heightBinding);
-        //}
-
-        //private void BindWidth(DocumentController docController, IEnumerable<DocumentController> docContextList)
-        //{
-        //    var widthController = docController.GetDereferencedField(DashConstants.KeyStore.WidthFieldKey, docContextList) as NumberFieldModelController;
-        //    Debug.Assert(widthController != null);
-
-        //    var widthBinding = new Binding
-        //    {
-        //        Source = widthController,
-        //        Path = new PropertyPath(nameof(widthController.Data)),
-        //        Mode = BindingMode.TwoWay,
-        //        UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
-        //    };
-        //    xWidthTextBox.SetBinding(TextBox.TextProperty, widthBinding);
-        //}
     }
 }
