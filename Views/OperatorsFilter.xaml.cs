b2df0adea18d36fd8ad2bffdb235ec882013369b
Crystal Chen <hui_chen@brown.edu>
2017-07-20 15:42:43 -0400

    almost fully working, suggestion selection doesn't search properly

--- Views/OperatorsFilter.xaml.cs
+++ Views/OperatorsFilter.xaml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -21,14 +22,14 @@ namespace Dash
 {
     public sealed partial class OperatorsFilter : UserControl
     {
-        ObservableCollection<OperatorFieldModel> Arithmetics = new ObservableCollection<OperatorFieldModel>() { new OperatorFieldModel("Divide") };
-        ObservableCollection<OperatorFieldModel> Sets = new ObservableCollection<OperatorFieldModel>() { new OperatorFieldModel("Union"), new OperatorFieldModel("Intersection") };
-        ObservableCollection<OperatorFieldModel> Maps = new ObservableCollection<OperatorFieldModel> () { new OperatorFieldModel("ImageToUri") };
-        ObservableCollection<OperatorFieldModel> All = new ObservableCollection<OperatorFieldModel>() { new OperatorFieldModel("Divide"), new OperatorFieldModel("Union"), new OperatorFieldModel("Intersection"), new OperatorFieldModel("ImageToUri") };
-        ObservableCollection<OperatorFieldModel> FilteredArithmetics { get; set; }
-        ObservableCollection<OperatorFieldModel> FilteredSets { get; set; }
-        ObservableCollection<OperatorFieldModel> FilteredMaps { get; set; }
-        ObservableCollection<OperatorFieldModel> FilteredAll { get; set; }
+        List<string> arithmetics = new List<string>() {"Divide"};
+        List<string> sets = new List<string>() { "Union", "Intersection" };
+        List<string> maps = new List<string> () { "ImageToUri" };
+        List<string> all = new List<string>() { "Divide", "Union", "Intersection", "ImageToUri"};
+        //ObservableCollection<OperatorFieldModel> Arithmetics { get; set; }
+        //ObservableCollection<OperatorFieldModel> Sets { get; set; }
+        //ObservableCollection<OperatorFieldModel> Maps { get; set; }
+        //ObservableCollection<OperatorFieldModel> All { get; set; }
         private static OperatorsFilter instance;
         public static OperatorsFilter Instance
         {
@@ -44,17 +45,109 @@ namespace Dash
         private OperatorsFilter()
         {
             this.InitializeComponent();
-            FilteredAll = All;
-            FilteredMaps = Maps;
-            FilteredSets = Sets;
-            FilteredArithmetics = Arithmetics;
+            //All = MakeCategory(all);
+            //Maps = MakeCategory(maps);
+            //Sets = MakeCategory(sets);
+            //Arithmetics = MakeCategory(arithmetics);
             this.SetManipulation();
-            xAllList.SelectionChanged += delegate { this.SelectionChanged(xAllList); };
-            xArithmeticList.SelectionChanged += delegate { this.SelectionChanged(xArithmeticList); };
-            xMapList.SelectionChanged += delegate { this.SelectionChanged(xMapList); };
-            xSetList.SelectionChanged += delegate { this.SelectionChanged(xSetList); };
+            xAllList.Tapped += delegate { this.ItemsSelected(xAllList); };
+            xArithmeticList.Tapped += delegate { this.ItemsSelected(xArithmeticList); };
+            xMapList.Tapped += delegate { this.ItemsSelected(xMapList); };
+            xSetList.Tapped += delegate { this.ItemsSelected(xSetList); };
+            xSearch.TextChanged += XSearch_TextChanged;
+            xSearch.QuerySubmitted += XSearch_QuerySubmitted;
         }
 
+        private void XSearch_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            this.UpdateList(args.QueryText);
+        }
+
+        //private ObservableCollection<OperatorFieldModel> MakeCategory(List<string> operatorTypes)
+        //{
+        //    ObservableCollection<OperatorFieldModel> operators = new ObservableCollection<OperatorFieldModel>();
+        //    foreach(var type in operatorTypes)
+        //    {
+        //        operators.Add(new OperatorFieldModel(type));
+        //    }
+        //    return operators;
+        //}
+
+        private void XSearch_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            this.UpdateList(args.SelectedItem as string);
+        }
+
+        private void UpdateList(string query)
+        {
+            var results = GetMatches(query);
+            var listView = GetCurrentListView();
+            listView.ItemsSource = results;
+        }
+
+        private void XSearch_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                if (sender.Text.Length > 0)
+                {
+                    sender.ItemsSource = GetMatches(sender.Text);
+                } else
+                {
+                    xAllList.ItemsSource = all;
+                    xArithmeticList.ItemsSource = arithmetics;
+                    xSetList.ItemsSource = sets;
+                    xMapList.ItemsSource = maps;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the list view in the current pivot
+        /// </summary>
+        /// <returns></returns>
+        private ListView GetCurrentListView()
+        {
+            ListView currentListView = null;
+            var selectedItem = xRootPivot.SelectedItem;
+            currentListView = selectedItem == xAllTab ? xAllList : currentListView;
+            currentListView = selectedItem == xArithmeticTab ? xArithmeticList : currentListView;
+            currentListView = selectedItem == xSetTab ? xSetList : currentListView;
+            currentListView = selectedItem == xMapTab ? xMapList : currentListView;
+            return currentListView; 
+        }
+
+        /// <summary>
+        /// Returns operator types that match the query
+        /// </summary>
+        /// <param name="searchInput"></param>
+        /// <returns></returns>
+        private List<string> GetMatches(string searchInput)
+        {
+            List<string> list = null;
+            list = xRootPivot.SelectedItem == xAllTab ? all:list;
+            list = xRootPivot.SelectedItem == xArithmeticTab ? arithmetics:list;
+            list = xRootPivot.SelectedItem == xMapTab ? maps : list;
+            list = xRootPivot.SelectedItem == xSetTab ? sets : list;
+            List<string> suggestions = new List<string>();
+            if (list != null)
+            {
+                foreach(var item in list)
+                {
+                    var type = item.ToLower();
+                    var input = searchInput.ToLower();
+                    if (type.Contains(input))
+                    {
+                        suggestions.Add(item);
+                    }
+                }
+            }
+            return suggestions;
+        }
+
+        /// <summary>
+        /// Adds handler to move the control
+        /// </summary>
         private void SetManipulation()
         {
             ManipulationMode = ManipulationModes.All;
@@ -69,21 +162,33 @@ namespace Dash
                 }
             };
         }
-        private void SelectionChanged(ListView listView)
+
+        /// <summary>
+        /// Creates and adds operator to workspace
+        /// </summary>
+        /// <param name="listView"></param>
+        private void ItemsSelected(ListView listView)
         {
-            var model = listView.SelectedItem as OperatorFieldModel;
+            var model = (string)listView.SelectedItem;
             var docController = this.CreateOperator(model);
-            MainPage.Instance.DisplayDocument(docController);
+            if (docController != null)
+                MainPage.Instance.DisplayDocument(docController);
         }
 
-        private DocumentController CreateOperator(OperatorFieldModel model)
+        /// <summary>
+        /// Generates operator according to the item selected in the list view
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        private DocumentController CreateOperator(string type)
         {
             DocumentController opModel = null;
-            if (model.Type == "Divide")
+            if (type == null) return null;
+            if (type == "Divide")
             {
                 opModel =
                 OperatorDocumentModel.CreateOperatorDocumentModel(
-                    new DivideOperatorFieldModelController(model));
+                    new DivideOperatorFieldModelController(new OperatorFieldModel(type)));
                 var view = new DocumentView
                 {
                     Width = 200,
@@ -92,11 +197,11 @@ namespace Dash
                 var opvm = new DocumentViewModel(opModel);
                 //OperatorDocumentViewModel opvm = new OperatorDocumentViewModel(opModel);
                 view.DataContext = opvm;
-            } else if (model.Type == "Union")
+            } else if (type == "Union")
             {
                 opModel =
                     OperatorDocumentModel.CreateOperatorDocumentModel(
-                        new UnionOperatorFieldModelController(model));
+                        new UnionOperatorFieldModelController(new OperatorFieldModel(type)));
                 var unionView = new DocumentView
                 {
                     Width = 200,
@@ -104,12 +209,12 @@ namespace Dash
                 };
                 var unionOpvm = new DocumentViewModel(opModel);
                 unionView.DataContext = unionOpvm;
-            } else if (model.Type == "Intersection")
+            } else if (type == "Intersection")
             {
                 // add union operator for testing 
                 opModel =
                     OperatorDocumentModel.CreateOperatorDocumentModel(
-                        new IntersectionOperatorModelController(model));
+                        new IntersectionOperatorModelController(new OperatorFieldModel(type)));
                 var intersectView = new DocumentView
                 {
                     Width = 200,
@@ -117,12 +222,12 @@ namespace Dash
                 };
                 var intersectOpvm = new DocumentViewModel(opModel);
                 intersectView.DataContext = intersectOpvm;
-            } else if (model.Type == "ImageToUri")
+            } else if (type == "ImageToUri")
             {
                 // add image url -> image operator for testing
                 opModel =
                     OperatorDocumentModel.CreateOperatorDocumentModel(
-                        new ImageOperatorFieldModelController(model));
+                        new ImageOperatorFieldModelController(new OperatorFieldModel(type)));
                 var imgOpView = new DocumentView
                 {
                     Width = 200,
@@ -132,6 +237,20 @@ namespace Dash
                 imgOpView.DataContext = imgOpvm;
             }
             return opModel;
+        }
+
+        /// <summary>
+        /// Hightlights selected pivot
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void xRootPivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            xAllBorder.Background = xRootPivot.SelectedItem == xAllTab ? new SolidColorBrush(Colors.SteelBlue) : new SolidColorBrush(Colors.Gray);
+            xArithmeticBorder.Background = xRootPivot.SelectedItem == xArithmeticTab ? new SolidColorBrush(Colors.SteelBlue) : new SolidColorBrush(Colors.Gray);
+            xMapBorder.Background = xRootPivot.SelectedItem == xMapTab ? new SolidColorBrush(Colors.SteelBlue) : new SolidColorBrush(Colors.Gray);
+            xSetBorder.Background = xRootPivot.SelectedItem == xSetTab ? new SolidColorBrush(Colors.SteelBlue) : new SolidColorBrush(Colors.Gray);
+            xCustomBorder.Background = xRootPivot.SelectedItem == xCustomTab ? new SolidColorBrush(Colors.SteelBlue) : new SolidColorBrush(Colors.Gray);
         }
     }
 
