af9c7163e2705dc2a0f1cd1131f3cde73314b6ef
Luke Murray <luke_murray@brown.edu>
2017-07-20 11:12:51 -0400

    Started working on document settings factory

--- Util/DocumentExtensions.cs
+++ Util/DocumentExtensions.cs
@@ -28,7 +28,7 @@ namespace Dash
         /// <summary>
         /// Gets the layout list which should always be in the deepestPrototype for the document
         /// </summary>
-        private static DocumentCollectionFieldModelController GetLayoutList(this DocumentController doc, Context context)
+        public static DocumentCollectionFieldModelController GetLayoutList(this DocumentController doc, Context context)
         {
             context = Context.SafeInitAndAddDocument(context, doc);
             var layoutList = doc.GetField(DashConstants.KeyStore.LayoutListKey) as DocumentCollectionFieldModelController;
