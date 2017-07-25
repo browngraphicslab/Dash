0a1e7a9964202eaec6c7acfd354b21308b9e929c
Galen Hall <galen_hall@brown.edu>
2017-07-24 20:18:24 -0400

    Added FilterView class; not filled in yet

---
+++ Views/FilterView.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Dash.Views
+{
+    public sealed partial class FilterView : UserControl
+    {
+        public FilterView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void XFieldBox_OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void xExpandMainGrid_Completed(object sender, object e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void fieldEquals_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void fieldContains_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void hasField_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void xCollapseMainGrid_Completed(object sender, object e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void xSearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
