c7a2c0ba85f9bc9d9759122758a87736572c2b9b
= <ellen.z.jiang@gmail.com>
2017-07-26 14:54:49 -0400

    gridview spacing added

--- Views/InterfaceBuilder/SettingsPaneFromDocumentControllerFactory.cs
+++ Views/InterfaceBuilder/SettingsPaneFromDocumentControllerFactory.cs
@@ -32,13 +32,17 @@ namespace Dash
             {
                 return CreateRichTextSettingsLayout(layoutDocument);
             }
-            if (type == DashConstants.DocumentTypeStore.FreeFormDocumentLayout || type == GridViewLayout.DocumentType /*|| type == ListViewLayout.DocumentType*/)
+            if (type == DashConstants.DocumentTypeStore.FreeFormDocumentLayout /*|| type == GridViewLayout.DocumentType || type == ListViewLayout.DocumentType*/)
             {
                 return CreateDocumentSettingsLayout(layoutDocument, dataDocument);
             }
             if (type == ListViewLayout.DocumentType)
             {
                 return CreateListViewSettingsLayout(layoutDocument);
+            }
+            if (type == GridViewLayout.DocumentType)
+            {
+                return CreateGridViewsettingsLayout(layoutDocument); 
             }
 
 
@@ -69,6 +73,11 @@ namespace Dash
         {
             var context = new Context(); // bcz: ??? Is this right?
             return new ListViewSettings(layoutDocument, context);
+        }
+
+        private static UIElement CreateGridViewsettingsLayout(DocumentController layoutDocument)
+        {
+            return new GridViewSettings(layoutDocument, new Context()); 
         }
 
         private static UIElement CreateTextSettingsLayout(DocumentController layoutDocument)
