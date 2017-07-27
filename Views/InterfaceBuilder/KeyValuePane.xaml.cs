ec59446f1b31f6f35277139513f118b7fd2398ad
= <kyu_bin_kwon@brown.edu>
2017-07-27 17:10:17 -0400

    ui kinda works but the actual adding functionality not added

--- Views/InterfaceBuilder/KeyValuePane.xaml.cs
+++ Views/InterfaceBuilder/KeyValuePane.xaml.cs
@@ -7,6 +7,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using DashShared;
 using System;
+using Windows.UI.Xaml.Media.Imaging;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -19,11 +20,16 @@ namespace Dash
         private DocumentController _documentControllerDataContext;
         private ObservableCollection<KeyFieldContainer> ListItemSource { get; }
 
+        private bool _addKVPaneOpen = true;
+
         public KeyValuePane()
         {
             InitializeComponent();
             ListItemSource = new ObservableCollection<KeyFieldContainer>();
             DataContextChanged += KeyValuePane_DataContextChanged;
+
+            ToggleAddKVPane();
+            xTypeComboBox.ItemsSource = Enum.GetValues(typeof(TypeInfo));
         }
 
         public void SetDataContextToDocumentController(DocumentController documentToDisplay)
@@ -60,6 +66,32 @@ namespace Dash
             }
         }
 
+        private void AddButton_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            ToggleAddKVPane();
+        }
+
+        private void ToggleAddKVPane()
+        {
+            _addKVPaneOpen = !_addKVPaneOpen;
+            if (_addKVPaneOpen)
+            {
+                xAddButton.Content = "⏎";
+                xNewKeyField.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                xTypeComboBox.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                xNewValueField.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            }
+            else
+            {
+                xAddButton.Content = "+";
+                xNewKeyField.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                xTypeComboBox.IsEnabled = false;
+                xTypeComboBox.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                xNewValueField.IsEnabled = false;
+                xNewValueField.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                xDefaultImage.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            }
+        }
         /// <summary>
         /// A container which represents a single row in the list created by the <see cref="KeyValuePane"/>
         /// </summary>
@@ -67,7 +99,6 @@ namespace Dash
         {
             public Key Key { get; }
             public FieldModelController Controller { get; }
-
             public string Type { get; }
 
             public KeyFieldContainer(Key key, FieldModelController controller)
@@ -77,24 +108,75 @@ namespace Dash
                 Type = (controller.TypeInfo).ToString();
             }
         }
-        
-        private void xKeyValueListView_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            
-            //var kv = e.ClickedItem as KeyFieldContainer;
-            //if (kv == _selectedKV)
-            //{
-            //    ResetKeyValueModifier();
-            //    return;
-            //}
-            //_selectedKV = kv;
 
-            //xNewValueField.Text = "< Enter new key";
-            //xNewKeyField.Text = _selectedKV.Key.Name;
-            //xNewValueField.IsEnabled = false;
+
+        private void xNewKeyField_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (xNewKeyField.Text != "") xTypeComboBox.IsEnabled = true;
+            else
+            {
+                xDefaultImage.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                xTypeComboBox.IsEnabled = false;
+            }
         }
 
+        private void xTypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = (TypeInfo)xTypeComboBox.SelectedItem;
+            if (item == TypeInfo.None)
+            {
+                xNewValueField.IsEnabled = false;
+                return;
+            }
+
+            xNewValueField.IsEnabled = true;
+            if (item == TypeInfo.Image)
+            {
+                xDefaultImage.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                xNewValueField.Text = ImageBox.DefaultImageUri.AbsoluteUri; 
+            }
+            else if (item == TypeInfo.Text || item == TypeInfo.Number)
+            {
+                xDefaultImage.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            }
+        }
+
+        private void xNewValueField_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string value = xNewValueField.Text; 
+            if (value == "") return;
+            var item = (TypeInfo)xTypeComboBox.SelectedItem;
+            if (item == TypeInfo.Image)
+            {
+                xDefaultImage.Source = new BitmapImage(new Uri(value)); 
+            }
+            else if (item == TypeInfo.Number)
+            {
+                double num;
+                if (!double.TryParse(value, out num))                             // if it's not a number
+                {
+                    return; 
+                }
+            }
+        }
+
+
         /* 
+        private void xKeyValueListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+            var kv = e.ClickedItem as KeyFieldContainer;
+            if (kv == _selectedKV)
+            {
+                ResetKeyValueModifier();
+                return;
+            }
+            _selectedKV = kv;
+
+            xNewValueField.Text = "< Enter new key";
+            xNewKeyField.Text = _selectedKV.Key.Name;
+            xNewValueField.IsEnabled = false;
+        }
         private void ValueField_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
         {
             if (e.Key == Windows.System.VirtualKey.Enter)
