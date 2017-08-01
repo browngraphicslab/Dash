255d23e1a4bf913504af6d17d40b1be818d2cf81
WINDOWS-TG53K0C\Administrator <david_wang1@brown.edu>
2017-08-01 17:38:07 -0400

    Collectionview fixes done

--- Views/CollectionView.xaml.cs
+++ Views/CollectionView.xaml.cs
@@ -23,47 +23,11 @@ namespace Dash
     public sealed partial class CollectionView : SelectionElement
     {
         public int MaxZ { get; set; }
-        //TODO move to freeform view
-        public bool CanLink
-        {
-            get
-            {
-                if (CurrentView is CollectionFreeformView)
-                    return (CurrentView as CollectionFreeformView).CanLink;
-                return false;
-            }
-            set
-            {
-                if (CurrentView is CollectionFreeformView)
-                    (CurrentView as CollectionFreeformView).CanLink = value;
-            }
-        }
-
-        // TODO not sure what these are used for and whether or not they should be defined here
-        public PointerRoutedEventArgs PointerArgs
-        {
-            get
-            {
-                if (CurrentView is CollectionFreeformView)
-                    return (CurrentView as CollectionFreeformView).PointerArgs;
-                return null;
-            }
-            set
-            {
-                if (CurrentView is CollectionFreeformView)
-                    (CurrentView as CollectionFreeformView).PointerArgs = value;
-            }
-
-        }
         public UserControl CurrentView { get; set; }
-        // TODO generate overlay menu for collections on the fly
-        private OverlayMenu _colMenu = null;
+        private OverlayMenu _colMenu;
         public CollectionViewModel ViewModel
         {
-            get
-            {
-                return DataContext as CollectionViewModel;
-            }
+            get { return DataContext as CollectionViewModel;}
             set { DataContext = value; }
         }
         public CollectionView ParentCollection { get; set; }
@@ -76,11 +40,8 @@ namespace Dash
             ViewModel = vm;
             CurrentView = new CollectionFreeformView();
             xContentControl.Content = CurrentView;
-            SetEventHandlers();
-            CanLink = true;
         }
 
-        // TODO create and remove this on the fly
         private void InitializeFlyout()
         {
             _flyout = new MenuFlyout();
@@ -123,32 +84,13 @@ namespace Dash
             DisposeFlyout();
         }
 
-        // TODO make all event handlers in xaml or add them here but not both
-        private void SetEventHandlers()
-        {
-            Loaded += CollectionView_Loaded;
-            DocumentViewContainerGrid.DragOver += CollectionGrid_DragOver;
-            DocumentViewContainerGrid.Drop += CollectionGrid_Drop;
-            ConnectionEllipse.ManipulationStarted += ConnectionEllipse_OnManipulationStarted;
-            Tapped += CollectionView_Tapped;
-        }
-
         private void CollectionView_Loaded(object sender, RoutedEventArgs e)
         {
             ParentDocument = this.GetFirstAncestorOfType<DocumentView>();
             ParentCollection = this.GetFirstAncestorOfType<CollectionView>();
-
-            // TODO fix this bug, and the shadow thing we were talking about
             if (ParentDocument == MainPage.Instance.MainDocView)
             {
                 ParentDocument.IsMainCollection = true;
-                //Temporary graphical hax.to be removed when collectionview menu moved to its document.
-                ParentDocument.XGrid.Background = new SolidColorBrush(Colors.Transparent);
-                ParentDocument.xBorder.Margin = new Thickness(ParentDocument.xBorder.Margin.Left + 5,
-                    ParentDocument.xBorder.Margin.Top + 5,
-                    ParentDocument.xBorder.Margin.Right,
-                    ParentDocument.xBorder.Margin.Bottom);
-                OpenMenu(); // TODO generate this on the fly
                 ParentSelectionElement?.SetSelectedElement(this);
                 xOuterGrid.BorderThickness = new Thickness(0);
             }
@@ -184,7 +126,7 @@ namespace Dash
 
         private void ConnectionEllipse_OnPointerPressed(object sender, PointerRoutedEventArgs e)
         {
-            string docId = (ParentDocument.DataContext as DocumentViewModel).DocumentController.GetId();
+            string docId = (ParentDocument.DataContext as DocumentViewModel)?.DocumentController.GetId();
             Ellipse el = sender as Ellipse;
             Key outputKey = DocumentCollectionFieldModelController.CollectionKey;
             IOReference ioRef = new IOReference(null, null, new DocumentFieldReference(docId, outputKey), true, e, el, ParentDocument); // TODO KB 
@@ -194,7 +136,7 @@ namespace Dash
 
         private void ConnectionEllipse_OnPointerReleased(object sender, PointerRoutedEventArgs e)
         {
-            string docId = (ParentDocument.DataContext as DocumentViewModel).DocumentController.GetId();
+            string docId = (ParentDocument.DataContext as DocumentViewModel)?.DocumentController.GetId();
             Ellipse el = sender as Ellipse;
             Key outputKey = DocumentCollectionFieldModelController.CollectionKey;
             IOReference ioRef = new IOReference(null, null, new DocumentFieldReference(docId, outputKey), false, e, el, ParentDocument); // TODO KB 
@@ -243,7 +185,7 @@ namespace Dash
                 e.AcceptedOperation = DataPackageOperation.Move;
         }
 
-        private async void CollectionGrid_Drop(object sender, DragEventArgs e)
+        private void CollectionGrid_Drop(object sender, DragEventArgs e)
         {
             
             if (e.DataView.Properties[RadialMenuView.RadialMenuDropKey] != null)
@@ -260,28 +202,7 @@ namespace Dash
             }
             e.Handled = true;
             RefreshItemsBinding();
-            foreach (var s in e.DataView.AvailableFormats)
-                Debug.Write("" + s);
-            if (e.DataView.Contains(StandardDataFormats.StorageItems))
-            {
-                var items = await e.DataView.GetStorageItemsAsync();
-                if (items.Count > 0)
-                {
-                    foreach (var i in items)
-                        if (i is Windows.Storage.StorageFile)
-                        {
-                            var storageFile = i as Windows.Storage.StorageFile;
-                            if (storageFile.ContentType.Contains("image"))
-                            {
-                                var bitmapImage = new Windows.UI.Xaml.Media.Imaging.BitmapImage();
-                                bitmapImage.SetSource(await storageFile.OpenAsync(Windows.Storage.FileAccessMode.Read));
-                                var doc = new AnnotatedImage(new Uri(i.Path), i.Name);
-                                (DataContext as CollectionViewModel).CollectionFieldModelController.AddDocument(doc.Document);
-                            }
-                        }
-                }
-            }
-            else if (ItemsCarrier.GetInstance().Source != null)
+            if (ItemsCarrier.GetInstance().Source != null)
             {
                 //var text = await e.DataView.GetTextAsync(StandardDataFormats.Html).AsTask();
                 ItemsCarrier.GetInstance().Destination = ViewModel;
@@ -310,17 +231,9 @@ namespace Dash
         }
 
         #endregion
-
-
-
-
         #region Menu
-        /// <summary>
-        /// Changes the view to the Freeform by making that Freeform visible in the CollectionView.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void SetFreeformView()
+
+        private void SetFreeformView()
         {
             if (CurrentView is CollectionFreeformView) return;
             ManipulationMode = ManipulationModes.All;
@@ -328,21 +241,21 @@ namespace Dash
             xContentControl.Content = CurrentView;
         }
 
-        public void SetListView()
+        private void SetListView()
         {
             if (CurrentView is CollectionListView) return;
             ManipulationMode = ManipulationModes.None;
             CurrentView = new CollectionListView(this);
-            (CurrentView as CollectionListView).HListView.SelectionChanged += ViewModel.SelectionChanged;
+            ((CollectionListView) CurrentView).HListView.SelectionChanged += ViewModel.SelectionChanged;
             xContentControl.Content = CurrentView;
         }
 
-        public void SetGridView()
+        private void SetGridView()
         {
             if (CurrentView is CollectionGridView) return;
             ManipulationMode = ManipulationModes.None;
             CurrentView = new CollectionGridView(this);
-            (CurrentView as CollectionGridView).xGridView.SelectionChanged += ViewModel.SelectionChanged;
+            ((CollectionGridView) CurrentView).xGridView.SelectionChanged += ViewModel.SelectionChanged;
             xContentControl.Content = CurrentView;
         }
 
@@ -356,23 +269,26 @@ namespace Dash
         private void CloseMenu()
         {
             var panel = _colMenu.Parent as Panel;
-            if (panel != null) panel.Children.Remove(_colMenu);
+            panel?.Children.Remove(_colMenu);
+            _colMenu.Dispose();
             _colMenu = null;
             xMenuColumn.Width = new GridLength(0);
         }
 
         private void SelectAllItems()
         {
-            if (CurrentView is CollectionGridView)
+            var view = CurrentView as CollectionGridView;
+            if (view != null)
             {
-                var gridView = (CurrentView as CollectionGridView).xGridView;
+                var gridView = view.xGridView;
                 if (gridView.SelectedItems.Count != ViewModel.DataBindingSource.Count)
                     gridView.SelectAll();
                 else gridView.SelectedItems.Clear();
             }
-            if (CurrentView is CollectionListView)
+            var currentView = CurrentView as CollectionListView;
+            if (currentView != null)
             {
-                var listView = (CurrentView as CollectionListView).HListView;
+                var listView = currentView.HListView;
                 if (listView.SelectedItems.Count != ViewModel.DataBindingSource.Count)
                     listView.SelectAll();
                 else
@@ -494,7 +410,6 @@ namespace Dash
             (CurrentView as CollectionFreeformView)?.UserControl_ManipulationDelta(sender, e);
         }
         #endregion
-
 
         private void CollectionView_OnPointerWheelChanged(object sender, PointerRoutedEventArgs e)
         {
