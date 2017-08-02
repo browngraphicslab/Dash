66d34094eff646f78aec0b8f2f86ad48c5d1a537
Crystal Chen <hui_chen@brown.edu>
2017-08-02 18:29:01 -0400

    bug: name editing textbox doesn't open up properly on newly added fields following the removal of field(s) from the key value pane, may no longer be a problem if field(s) can be deleted from a document and data context of the listview can be updated to reflect the underlying changes after deletion (as opposed to removing items from the listview)

--- Views/InterfaceBuilder/KeyValuePane.xaml.cs
+++ Views/InterfaceBuilder/KeyValuePane.xaml.cs
@@ -1,14 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
 using DashShared;
-using System;
+using Visibility = Windows.UI.Xaml.Visibility;
 
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace Dash
 {
@@ -17,7 +28,43 @@ namespace Dash
         public static readonly string DragPropertyKey = "key";
 
         private DocumentController _documentControllerDataContext;
+
         private ObservableCollection<KeyFieldContainer> ListItemSource { get; }
+
+        /// <summary>
+        /// The KeyFieldContainer that is currently selected on the listview
+        /// </summary>
+        private KeyFieldContainer _selectedKV;
+
+        /// <summary>
+        /// Currently visible name editing TextBox
+        /// </summary>
+        private TextBox _visibleEditBox;
+
+        /// <summary>
+        /// Currently visible type ComboBox
+        /// </summary>
+        private ComboBox _visibleTypeComboBox;
+
+        /// <summary>
+        /// The most recently added field
+        /// </summary>
+        private KeyFieldContainer _newField;
+
+        /// <summary>
+        /// List of all Grids around items in the listview
+        /// </summary>
+        List<Grid> _gridContainers = new List<Grid>();
+
+        /// <summary>
+        /// List of all name editing TextBoxes in the listview
+        /// </summary>
+        private List<TextBox> _textBoxes = new List<TextBox>();
+
+        /// <summary>
+        /// List of all type selection ComboBoxes
+        /// </summary>
+        private List<ComboBox> _typeComboBoxes = new List<ComboBox>();
 
         public KeyValuePane()
         {
@@ -44,6 +91,9 @@ namespace Dash
             ListItemSource.Clear();
             foreach (var keyFieldPair in _documentControllerDataContext.EnumFields())
                 ListItemSource.Add(new KeyFieldContainer(keyFieldPair.Key, keyFieldPair.Value));
+            _visibleEditBox = null;
+            _selectedKV = null;
+            _newField = null;
         }
 
 
@@ -60,104 +110,256 @@ namespace Dash
             }
         }
 
-        private void ValueField_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        /// <summary>
+        /// Shows delete buttons (which only remove items from the list view at the moment and the "deleted" items show up again whenever the datacontext updates)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void XEditButton_OnTapped(object sender, TappedRoutedEventArgs e)
         {
-            if (e.Key == Windows.System.VirtualKey.Enter)
+            foreach (var grid in _gridContainers)
             {
-                if (!xNewKeyField.IsEnabled && _selectedKV != null)
-                {
-                    TextFieldModelController textCont = _selectedKV.Controller as TextFieldModelController;
-                    if ((_selectedKV.Controller as TextFieldModelController).Data != xNewValueField.Text || _selectedKV.Key.Name != xNewKeyField.Text)
-                    {
-                        _selectedKV.Key.Name = xNewKeyField.Text;
-                        (_selectedKV.Controller as TextFieldModelController).Data = xNewValueField.Text;
-                        SetListItemSourceToCurrentDataContext();
-                    }
-                    ResetKeyValueModifier();
-
-                    return;
-                }
-                if (xNewKeyField.Text == "" || xNewValueField.Text == "")
-                    return;
-
-                //var key = new Key(Guid.NewGuid().ToString(), (xNewKeyField as TextBox).Text); // TODO commented out cos i didn't want to waste guids on testing 
-                var key = new Key((new Random()).Next(0, 100000000).ToString(), (xNewKeyField as TextBox).Text);
-                var cont = new TextFieldModelController((sender as TextBox).Text);
-                ListItemSource.Add(new KeyFieldContainer(key, cont));
-
-                _documentControllerDataContext.SetField(key, cont, true);
-                xNewKeyField.Text = "";
-                xNewValueField.Text = "";
+                grid.ColumnDefinitions[0].Width = new GridLength(35);
             }
-            else if (e.Key == Windows.System.VirtualKey.Tab)
-            {
-                ResetKeyValueModifier();
-            }
+            xColumnBackground.ColumnDefinitions[0].Width = new GridLength(35);
+            xConfirmButton.Visibility = Visibility.Visible;
+            xEditButton.Visibility = Visibility.Collapsed;
         }
 
-        private void KeyField_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        /// <summary>
+        /// Hide delete buttons
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void XConfirmButton_OnTapped(object sender, TappedRoutedEventArgs e)
         {
-            if (_selectedKV == null) return;
-            if (!xNewValueField.IsEnabled && (e.Key == Windows.System.VirtualKey.Enter || e.Key == Windows.System.VirtualKey.Tab))
+            foreach (var grid in _gridContainers)
             {
-                var textCont = _selectedKV.Controller as TextFieldModelController;
-                if (textCont != null)
-                {
-                    xNewValueField.IsEnabled = true;
-                    xNewValueField.Text = textCont.Data;
-                    xNewKeyField.IsEnabled = false;
-                }
-                else
-                {
-                    if (_selectedKV.Key.Name != xNewKeyField.Text)
-                    {
-                        _selectedKV.Key.Name = xNewKeyField.Text; 
-                        SetListItemSourceToCurrentDataContext();
-                    }
-                    ResetKeyValueModifier();
-                }
+                grid.ColumnDefinitions[0].Width = new GridLength(0);
             }
+            xColumnBackground.ColumnDefinitions[0].Width = new GridLength(0);
+            xEditButton.Visibility = Visibility.Visible;
+            xConfirmButton.Visibility = Visibility.Collapsed;
         }
-
-        private KeyFieldContainer _selectedKV;
 
         private void xKeyValueListView_ItemClick(object sender, ItemClickEventArgs e)
         {
             var kv = e.ClickedItem as KeyFieldContainer;
-            if (kv == _selectedKV)
-            {
-                ResetKeyValueModifier();
-                return;    
-            }
-            _selectedKV = kv; 
 
-            xNewValueField.Text = "< Enter new key";
-            xNewKeyField.Text = _selectedKV.Key.Name;
-            xNewValueField.IsEnabled = false;
+            // control visibility of name editing TextBox and save name editing changes accordingly
+            if (_visibleEditBox != null)
+            {
+                // tapping on any item collapses the previously visible name edit text box
+                _visibleEditBox.Visibility = Visibility.Collapsed;
+                // tapping on any item updates the name of a field if it was in the process of being edited
+                if (_selectedKV?.Key.Name != _visibleEditBox.Text)
+                {
+                    _selectedKV.Key.Name = _visibleEditBox.Text;
+                    SetListItemSourceToCurrentDataContext();
+                }
+                _visibleEditBox = null;
+            }
+            _selectedKV = kv;
+
+            // Make the type ComboBox of the selected item visible and collapse all other type ComboBoxes
+            if (_selectedKV != null)
+            foreach (var box in _typeComboBoxes)
+            {
+                if (box.Tag.Equals(_selectedKV.HashCode))
+                {
+                    _visibleTypeComboBox = box;
+                    box.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    box.Visibility = Visibility.Collapsed;
+                }
+            }
         }
 
-        private void ResetKeyValueModifier()
+        /// <summary>
+        /// Removes item from listview (does not remove field from document)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void XDeleteButton_OnTapped(object sender, TappedRoutedEventArgs e)
         {
-            xNewKeyField.IsEnabled = true;
-            xNewValueField.IsEnabled = true;
-            xNewValueField.Text = "";
-            xNewKeyField.Text = "";
-            _selectedKV = null;
+            var button = sender as Button;
+            KeyFieldContainer targetKV = null;
+            if (button != null)
+            foreach (var item in ListItemSource)
+            {
+                if (button.Tag.Equals(item.HashCode))
+                {
+                    // the item whose hashcode matches the tag of the delete button is the target item to be removed from the list view
+                    targetKV = item;
+                }
+            }
+            ListItemSource.Remove(targetKV);
+            // TODO: Actually remove field from doc?
+        }
+
+        /// <summary>
+        /// Adds field to document
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void XAddButton_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            //var key = new Key(Guid.NewGuid().ToString(), (xNewKeyField as TextBox).Text); // TODO commented out cos i didn't want to waste guids on testing 
+            var key = new Key((new Random()).Next(0, 100000000).ToString(), "");
+            key.Name = "";
+            var cont = new TextFieldModelController(string.Empty);
+            _newField = new KeyFieldContainer(key, cont);
+            ListItemSource.Add(_newField);
+            _documentControllerDataContext.SetField(key, cont, true);
+        }
+
+        /// <summary>
+        /// Keeps track of all Grids in the ListView to control the visibility of the delete Buttons
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void XItemGridContainer_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _gridContainers.Add(sender as Grid);
+        }
+
+        /// <summary>
+        /// Keeps track of all name editing TextBoxes in the ListView
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void XEditBox_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            _textBoxes.Add(textBox);
+
+            // opens up name editing textbox and forces focus on it programmatically to prompt user to name the new field
+            if (textBox.Tag.Equals(_newField?.HashCode))
+            {
+                textBox.Visibility = Visibility.Visible;
+                textBox.Focus(FocusState.Programmatic);
+                foreach (var item in ListItemSource)
+                {
+                    if (item.HashCode.Equals(textBox.Tag))
+                    {
+                        _selectedKV = item;
+                    }
+                }
+                if (_visibleEditBox != null)
+                {
+                    _visibleEditBox.Visibility = Visibility.Collapsed;
+                }
+                if (_visibleTypeComboBox != null)
+                {
+                    _visibleTypeComboBox.Visibility = Visibility.Collapsed;
+                }
+                _visibleEditBox = textBox;
+                _visibleTypeComboBox = null;
+            }
+        }
+
+        /// <summary>
+        /// Updates name of field on enter/tab
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void xNameEditBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter || e.Key == Windows.System.VirtualKey.Tab)
+            {
+                if (_selectedKV.Key.Name != (sender as TextBox).Text)
+                {
+                    _selectedKV.Key.Name = (sender as TextBox).Text;
+                    _visibleEditBox.Visibility = Visibility.Collapsed;
+                    _visibleEditBox = null;
+                    SetListItemSourceToCurrentDataContext();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Make visible the name editing TextBox of the item whose name TextBlock is being tapped on and collapse 
+        /// the previously visible TextBox (without updating the name even if changes were made)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void xNameTextBlock_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            foreach (var textBox in _textBoxes)
+            {
+                if (textBox.Tag.Equals(_selectedKV?.HashCode))
+                {
+                    // collapse the textbox that was previously visible
+                    if (_visibleEditBox != null)
+                    {
+                        _visibleEditBox.Visibility = Visibility.Collapsed;
+                    }
+                    // keeps track of the textbox that is currently visible
+                    _visibleEditBox = textBox;
+                    textBox.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Keeps track of all type selection ComboBoxes to control their visibility (visible only when item is selected)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void XTypeCombo_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _typeComboBoxes.Add(sender as ComboBox);
+        }
+
+        /// <summary>
+        /// Changes the type of a field (not exactly working, throws errors when trying to change type of preexisting fields)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItem = (sender as ComboBox).SelectedValue;
+            if (selectedItem != null)
+            {
+                if (selectedItem.Equals(TypeInfo.Image))
+                {
+                    _selectedKV.Controller = new ImageFieldModelController(null);
+                }
+                else if (selectedItem.Equals(TypeInfo.Text))
+                {
+                    _selectedKV.Controller = new TextFieldModelController(string.Empty);
+                }
+                else if (selectedItem.Equals(TypeInfo.Number))
+                {
+                    _selectedKV.Controller = new NumberFieldModelController(double.NaN);
+                }
+                _documentControllerDataContext.SetField(_selectedKV.Key, _selectedKV.Controller, true);
+                SetListItemSourceToCurrentDataContext();
+            }
         }
     }
 
     /// <summary>
     /// A container which represents a single row in the list created by the <see cref="KeyValuePane"/>
     /// </summary>
-//    public class KeyFieldContainer
-//    {
-//        public Key Key { get; }
-//        public FieldModelController Controller { get; }
-//
-//        public KeyFieldContainer(Key key, FieldModelController controller)
-//        {
-//            Key = key;
-//            Controller = controller;
-//        }
-//    }
+    public class KeyFieldContainer
+    {
+        public Key Key { get; }
+        /// <summary>
+        /// Something for controls in the list to bind to to make them identifiable from the datatemplate
+        /// </summary>
+        public int HashCode { get { return Key.GetHashCode(); } }
+        public FieldModelController Controller { get; set; }
+        public Array Types { get; }
+        
+
+        public KeyFieldContainer(Key key, FieldModelController controller)
+        {
+            Key = key;
+            Controller = controller;
+            Types = Enum.GetValues(typeof(TypeInfo));
+        }
+    }
 }
