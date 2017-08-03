43615d56ee3fbc2550a99fdc19b47e75c393a658
Luke Murray <luke_murray@brown.edu>
2017-08-03 11:06:24 -0400

    updated the free form collection view model to implement part of the new interface

--- ViewModels/FreeFormCollectionViewModel.cs
+++ ViewModels/FreeFormCollectionViewModel.cs
@@ -29,35 +29,31 @@ namespace Dash
         {
             foreach (var docController in documents)
             {
-                var docVm = new DocumentViewModel(docController, IsInterfaceBuilder);
-                DocumentViewModels.Add(docVm);
+                AddDocument(docController, context);
             }
         }
 
         public void AddDocument(DocumentController document, Context context)
         {
-            throw new NotImplementedException();
+            var docVm = new DocumentViewModel(document, IsInterfaceBuilder);
+            DocumentViewModels.Add(docVm);
         }
 
         public void RemoveDocuments(List<DocumentController> documents)
         {
-            var docsToRemove = new HashSet<string>(documents.Select(doc => doc.GetId()).ToList());
-
-            foreach (var id in docsToRemove)
+            foreach (var doc in documents)
             {
-                var vmToRemove = DocumentViewModels.FirstOrDefault(vm => vm.DocumentController.GetId() == id);
-                if (vmToRemove != null)
-                {
-                    DocumentViewModels.Remove(vmToRemove);
-                }
-
+                RemoveDocument(doc);
             }
-
         }
 
         public void RemoveDocument(DocumentController document)
         {
-            throw new NotImplementedException();
+            var vmToRemove = DocumentViewModels.FirstOrDefault(vm => vm.DocumentController.GetId() == document.GetId());
+            if (vmToRemove != null)
+            {
+                DocumentViewModels.Remove(vmToRemove);
+            }
         }
     }
 }
