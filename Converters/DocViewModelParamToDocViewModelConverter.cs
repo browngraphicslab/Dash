f2024701847aab84b134ef473de9a8164a0bd6ca
Tyler Schicke <tyler_schicke@brown.edu>
2017-08-09 16:58:53 -0400

    Removed debug line

--- Converters/DocViewModelParamToDocViewModelConverter.cs
+++ Converters/DocViewModelParamToDocViewModelConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Dash
 {
     public class DocViewModelParamToDocViewModelConverter : SafeDataToXamlConverter<DocumentViewModelParameters, DocumentViewModel>
     {
+
         public override DocumentViewModel ConvertDataToXaml(DocumentViewModelParameters data, object parameter = null)
         {
             return new DocumentViewModel(data.Controller, data.IsInInterfaceBuilder, data.Context);
