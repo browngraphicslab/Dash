024abf7cbd4156e60aecabe251859c6c4640390e
= <kyu_bin_kwon@brown.edu>
2017-08-10 10:50:07 -0400

    made editabletextblock a xaml page

---
+++ Util/EditableTextBlock.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Dash
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public partial class EditableTextBlock
+    {
+        public TextBox Box {
+            get { return xTextBox; }
+            set { xTextBox = value; }
+        }
+
+        public TextBlock Block {
+            get { return xTextBlock; }
+            set { xTextBlock = value; }
+        }
+
+        public EditableTextBlock()
+        {
+            InitializeComponent();
+
+            Box.PointerWheelChanged += (s, e) => e.Handled = true;
+            Box.ManipulationDelta += (s, e) => e.Handled = true;
+
+            Box.LostFocus += (s, e) =>
+            {
+                Box.Visibility = Visibility.Collapsed;
+                Block.Visibility = Visibility.Visible;
+            };
+
+            Block.DoubleTapped += (s, e) =>
+            {
+                e.Handled = true;
+                Block.Visibility = Visibility.Collapsed;
+                Box.Visibility = Visibility.Visible;
+                Box.Focus(FocusState.Programmatic);
+                Box.SelectAll();
+            };
+        }
+    }
+}
