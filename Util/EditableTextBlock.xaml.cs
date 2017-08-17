a74f282b6a30d7072666036886ca30cc7f70c767
Tyler Schicke <tyler_schicke@brown.edu>
2017-08-17 17:19:16 -0400

    Added dependency properties to EditableTextBlock

--- Util/EditableTextBlock.xaml.cs
+++ Util/EditableTextBlock.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml;
+﻿using Windows.UI.Text;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media;
@@ -23,13 +24,13 @@ namespace Dash
         }
 
         #region BINDING PROPERTIES 
-        public static readonly DependencyProperty TextDependency = 
+        public static readonly DependencyProperty TextProperty = 
             DependencyProperty.Register("Text", typeof(string), typeof(EditableTextBlock), new PropertyMetadata(false));
 
         public string Text
         {
-            get { return (string)GetValue(TextDependency); }
-            set { SetValue(TextDependency, value); }
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
         }
 
         public static readonly DependencyProperty ColorDependency =
@@ -40,6 +41,34 @@ namespace Dash
             get { return (SolidColorBrush)GetValue(ColorDependency); }
             set { SetValue(ColorDependency, value); }
         }
+
+        public static readonly DependencyProperty FontSizeProperty = DependencyProperty.Register(
+            "FontSize", typeof(double), typeof(EditableTextBlock), new PropertyMetadata(default(double)));
+
+        public double FontSize
+        {
+            get { return (double) GetValue(FontSizeProperty); }
+            set { SetValue(FontSizeProperty, value); }
+        }
+
+        public static readonly DependencyProperty FontWeightPropertyProperty = DependencyProperty.Register(
+            "FontWeightProperty", typeof(FontWeight), typeof(EditableTextBlock), new PropertyMetadata(default(FontWeight)));
+
+        public FontWeight FontWeightProperty
+        {
+            get { return (FontWeight) GetValue(FontWeightPropertyProperty); }
+            set { SetValue(FontWeightPropertyProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextAlignmentPropertyProperty = DependencyProperty.Register(
+            "TextAlignmentProperty", typeof(TextAlignment), typeof(EditableTextBlock), new PropertyMetadata(default(TextAlignment)));
+
+        public TextAlignment TextAlignmentProperty
+        {
+            get { return (TextAlignment) GetValue(TextAlignmentPropertyProperty); }
+            set { SetValue(TextAlignmentPropertyProperty, value); }
+        }
+
 #endregion
 
         public EditableTextBlock()
