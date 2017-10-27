d3b84a13b31fa47c867cfdf54a09d86936a746d0
bobzel <zzzman@gmail.com>
2017-10-27 15:41:06 -0400

    header columns are resizable now.  schema field text boxes adjust dynamically.

--- Views/CollectionDBSchemaView.xaml.cs
+++ Views/CollectionDBSchemaView.xaml.cs
@@ -117,7 +117,7 @@ namespace Dash
             var field = dc.Document.GetDataDocument(null).GetDereferencedField(dc.HeaderViewModel.FieldKey, null);
             xEditTextBox.Text = field?.GetValue(null)?.ToString() ?? "<null>";
             var numReturns = xEditTextBox.Text.Count((c) => c == '\r');
-            xEditTextBox.Height = Math.Max(250, 50 + numReturns * 15);
+            xEditTextBox.Height = Math.Min(250, 50 + numReturns * 15);
             dc.Selected = true;
             xEditTextBox.SelectAll();
         }
