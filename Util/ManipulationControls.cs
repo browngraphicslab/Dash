b8ff51a3d1b0cc884f5fab90b1d329f94c2699ae
David Wang <david_wang1@brown.edu>
2017-12-10 17:32:52 -0500

    undid manipulation and courtesydocument interaction fixes. They make it imposible to drag out of fields

--- Util/ManipulationControls.cs
+++ Util/ManipulationControls.cs
@@ -86,18 +86,11 @@ namespace Dash
             }
             element.ManipulationMode = ManipulationModes.All;
             element.ManipulationStarted += ElementOnManipulationStarted;
-            element.PointerPressed += Element_PointerPressed;
         }
 
-        bool blockRightButtonManipulation = false;
-        private void Element_PointerPressed(object sender, PointerRoutedEventArgs e)
+        private void ElementOnManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
         {
-            blockRightButtonManipulation = e.GetCurrentPoint(MainPage.Instance).Properties.IsRightButtonPressed && _element is CollectionFreeformView;
-            _element.ManipulationMode = blockRightButtonManipulation ? ManipulationModes.None : ManipulationModes.All;
-        }
-            private void ElementOnManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
-        {
-            if (e.PointerDeviceType == BlockedInputType && FilterInput || _element.ManipulationMode == ManipulationModes.None)
+            if (e.PointerDeviceType == BlockedInputType && FilterInput)
             {
                 _processManipulation = false;
                 e.Handled = true;
