85819b691b54e2f67c110df01c20e3f6b1df022e
Trent <trent_green@brown.edu>
2018-01-05 14:42:22 -0500

    closes #219 but still has issues when dragging temporary images from web clippings that the application tries to copy from html cache

---
+++ ChromeExt/phils/App2/App2/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using IotWeb.Common;
+using IotWeb.Server;
+using UnicodeEncoding = System.Text.UnicodeEncoding;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private HttpServer s;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Grid_Drop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.Text))
+            {
+                var textJson = await e.DataView.GetTextAsync();
+                Debug.WriteLine(textJson);
+            }
+
+            var dropPos = e.GetPosition(null);
+            Debug.WriteLine("dropped at: " + dropPos);
+
+        }
+
+        private void UIElement_OnDragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+        }
+    }
+}
