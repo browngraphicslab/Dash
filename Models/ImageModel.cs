c854031ab52be540ae8e5c48cd040c43d9a73cc8
Luke Murray <luke_murray@brown.edu>
2018-01-27 23:30:35 -0500

    image operator might be universal

--- Models/ImageModel.cs
+++ Models/ImageModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using Windows.Storage;
 using DashShared;
 using Windows.UI.Xaml.Media.Imaging;
 using DashShared.Models;
@@ -14,7 +16,25 @@ namespace Dash
         /// <summary>
         /// A <see cref="Uri"/> which points to the <see cref="BitmapImage.UriSource"/>
         /// </summary>
-        public Uri Data;
+        public Uri Data
+        {
+            get => localFile == null ? globalUri : new Uri(ApplicationData.Current.LocalFolder.Path + localFile);
+            set
+            {
+                if (value.IsFile && value.LocalPath.Contains(ApplicationData.Current.LocalFolder.Path))
+                {
+                    localFile = value.Segments.Last();
+                }
+                else
+                {
+                    globalUri = value;
+                }
+            }
+        }
+
+        private string localFile;
+
+        private Uri globalUri;
 
         public string ByteData = null;
 
@@ -30,7 +50,7 @@ namespace Dash
         /// <param name="data">The uri that the image this field model encapsulates is sourced from</param>
         public ImageModel(Uri path, string bytes = null, string id = null) : base(id)
         {
-            Data = path;
+            Data = path;      
             ByteData = bytes;
         }
     }
