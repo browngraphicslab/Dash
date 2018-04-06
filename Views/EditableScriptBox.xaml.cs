646ac741885fd3bc66f34b51354893e6fcd39d88
Tyler Schicke <tyler_schicke@brown.edu>
2018-04-06 18:16:36 -0400

    Started kvp refactor

---
+++ Views/EditableScriptBox.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Dash.Annotations;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Dash
+{
+    public sealed partial class EditableScriptBox : INotifyPropertyChanged
+    {
+
+
+        private bool _textBoxLoaded;
+
+        private bool TextBoxLoaded
+        {
+            get => _textBoxLoaded;
+            set
+            {
+                _textBoxLoaded = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public EditableScriptViewModel ViewModel
+        {
+            get => DataContext as EditableScriptViewModel;
+            set => DataContext = value;
+        }
+
+
+        public EditableScriptBox()
+        {
+            this.InitializeComponent();
+        }
+
+        private void XTextBlock_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            e.Handled = true;
+            TextBoxLoaded = true;
+        }
+
+        private void XTextBox_OnPointerWheelChanged(object sender, PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void XTextBox_OnManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            //e.Handled = true;
+        }
+
+        private void XTextBox_OnKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+                SetExpression(XTextBox.Text);
+        }
+
+        private bool SetExpression(string text)
+        {
+            TextBoxLoaded = false;
+            XTextBlock.Visibility = Visibility.Visible;
+            try
+            {
+                FieldControllerBase field = OperatorScriptParser.Interpret(text);
+                ViewModel?.Reference.SetField(field, ViewModel.Context);
+            }
+            catch (OperatorScriptParser.ScriptException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private string GetRootExpression()
+        {
+            return ViewModel?.Reference.DereferenceToRoot(ViewModel.Context)?.GetValue(ViewModel.Context)?.ToString();
+        }
+
+        private string GetExpression()
+        {
+            return ViewModel?.Reference.Dereference(ViewModel.Context)?.GetValue(ViewModel.Context)?.ToString();
+        }
+
+        private void XTextBox_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxLoaded)
+            {
+                SetExpression(XTextBox.Text);
+            }
+        }
+
+        private void XTextBox_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            XTextBlock.Visibility = Visibility.Collapsed;
+            XTextBox.Focus(FocusState.Programmatic);
+            XTextBox.Text = GetExpression() ?? XTextBlock.Text;
+            XTextBox.SelectAll();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void EditableScriptBox_OnDataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+            if (ViewModel == null)
+            {
+                return;
+            }
+            FieldBinding<FieldControllerBase> binding = new FieldBinding<FieldControllerBase>
+            {
+                Document = ViewModel.Reference.GetDocumentController(ViewModel.Context),
+                Key = ViewModel.Reference.FieldKey,
+                Context = ViewModel.Context,
+                Converter = new ObjectToStringConverter(),
+                Mode = BindingMode.OneWay
+            };
+            XTextBlock.AddFieldBinding(TextBlock.TextProperty, binding);
+        }
+    }
+}
