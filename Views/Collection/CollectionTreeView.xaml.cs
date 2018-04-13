e1bd32487d8412b7f0d206118eb198c9f0bb4179
Luke Murray <luke_murray@brown.edu>
2018-04-12 20:58:10 -0400

    save pdf

--- Views/Collection/CollectionTreeView.xaml.cs
+++ Views/Collection/CollectionTreeView.xaml.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Reflection;
-using Windows.Foundation;
+using System.Reflection.Metadata;
+using System.Text;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using Dash.Models.DragModels;
 using Windows.ApplicationModel.DataTransfer;
+using Windows.Data.Pdf;
 using Windows.Storage;
 using Windows.Storage.Pickers;
+using Windows.Web.Http;
+using Gma.CodeCloud.Controls;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.IO;
 using Syncfusion.Pdf.Parsing;
+using Point = Windows.Foundation.Point;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -66,16 +74,72 @@ namespace Dash
 
         private async void MakePdf_OnTapped(object sender, TappedRoutedEventArgs e)
         {
+
+
+            String filename = "Sample.pdf";
+           // Windows.Storage.Streams.IInputStream stream =
+             //   await sampleFile.OpenReadAsync();
+
+          //  stream.Position = 0;
+
+            StorageFile stFile;
+            if (!(Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons")))
+            {
+                FileSavePicker savePicker = new FileSavePicker();
+                savePicker.DefaultFileExtension = ".pdf";
+                savePicker.SuggestedFileName = "Dash";
+                savePicker.FileTypeChoices.Add("Adobe PDF Document", new List<string>() { ".pdf" });
+                stFile = await savePicker.PickSaveFileAsync();
+                //await Windows.Storage.FileIO.WriteTextAsync(stFile, "Swift as a shadow");
+
+                // var stream = await stFile.OpenAsync(FileAccessMode.ReadWrite);
+
+                //  PdfDocument pdf = await PdfDocument.LoadFromStreamAsync(stream);
+                StorageFolder folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                StorageFile file = await folder.GetFileAsync("test.pdf");
+                PdfDocument pdf = await PdfDocument.LoadFromFileAsync(file);
+                //PdfDocument pdf = await PdfDocument.LoadFromFileAsync(stFile);
+                // PdfDocument pdf = pd.GetResults();
+                PdfPage page = pdf.GetPage(1);
+
+               
+
+            }
+            else
+            {
+                StorageFolder local = Windows.Storage.ApplicationData.Current.LocalFolder;
+                stFile = await local.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
+                await Windows.Storage.FileIO.WriteTextAsync(stFile, "Swift as a shadow");
+            }
+
+          
+
+           
+
+
+
+
+
+            //StreamReader rdr = new StreamReader(sampleFile);
+
+            //  PdfDocument pdf = new PdfDocument();
+
+            //  Document doc = new Document();
+            // PdfWriter.GetInstance(doc, new FileStream(txtOutput.Text, FileMode.Create));
+
+            /*
+
             //Load the PDF document as stream
+            Debug.WriteLine("t - " + typeof(MainPage).GetTypeInfo().Assembly);
             Stream pdfStream = typeof(MainPage).GetTypeInfo().Assembly.GetManifestResourceStream("Sample.Assets.Data.Sample.pdf");
 
             //Creates an empty PDF loaded document instance
             PdfLoadedDocument document = new PdfLoadedDocument();
 
             //Loads or opens an existing PDF document through Open method of PdfLoadedDocument class
-            await document.OpenAsync(pdfStream);
+            //await document.OpenAsync(pdfStream);
 
-            MemoryStream stream = new MemoryStream();
+            MemoryStream stream = new MemoryStream(100);
 
             await document.SaveAsync(stream);
 
@@ -87,11 +151,11 @@ namespace Dash
 
             Save(stream, "Result.pdf");
 
-           
 
-                //Load the PDF document.
+             //Load the PDF document.
 
-                PdfLoadedDocument loadedDocument = new PdfLoadedDocument("saved.pdf");
+              PdfLoadedDocument loadedDocument = new PdfLoadedDocument
+                  (Encoding.ASCII.GetBytes("saved.pdf"));
 
             //Get the loaded form.
 
@@ -109,7 +173,7 @@ namespace Dash
 
             //Close the document
 
-            loadedDocument.Close(true);
+            loadedDocument.Close(true); */
         }
 
         async void Save(Stream stream, string filename)
