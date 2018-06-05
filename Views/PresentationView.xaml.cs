ffae1450aa06df9c8e65a12139ac1012d6bb4849
julie <juliewang42@gmail.com>
2018-06-05 16:48:29 -0400

    added renaming functionality

--- Views/PresentationView.xaml.cs
+++ Views/PresentationView.xaml.cs
@@ -22,6 +22,8 @@ namespace Dash.Views
     {
         public PresentationViewModel ViewModel => DataContext as PresentationViewModel;
         public bool IsPresentationPlaying = false;
+        private PresentationViewTextBox _textbox;
+        private bool _giveTextBoxFocusUponFlyoutClosing = false;
 
         public PresentationView()
         {
@@ -32,6 +34,8 @@ namespace Dash.Views
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
             int selectedIndex = PinnedNodesListView.SelectedIndex;
+
+            // only move back if there is a step to go back to
             if (selectedIndex != 0)
             {
                 PinnedNodesListView.SelectedItem = PinnedNodesListView.Items[selectedIndex - 1];
@@ -41,10 +45,12 @@ namespace Dash.Views
 
         private void PlayStopButton_Click(object sender, RoutedEventArgs e)
         {
+            // can't play/stop if there's nothing in it
             if (PinnedNodesListView.Items.Count != 0)
             {
                 if (IsPresentationPlaying)
                 {
+                    // if it's currently playing, then it means the user just clicked the stop button. Reset.
                     IsPresentationPlaying = false;
                     PlayStopButton.Icon = new SymbolIcon(Symbol.Play);
                     PlayStopButton.Label = "Play";
@@ -52,22 +58,26 @@ namespace Dash.Views
                 }
                 else
                 {
+                    // zoom to first item in the listview
                     PinnedNodesListView.SelectionMode = ListViewSelectionMode.Single;
                     PinnedNodesListView.SelectedItem = PinnedNodesListView.Items[0];
-                    PlayStopButton.Label = "Stop";
                     NavigateToDocument((DocumentViewModel)PinnedNodesListView.SelectedItem);
 
                     IsPresentationPlaying = true;
                     PlayStopButton.Icon = new SymbolIcon(Symbol.Stop);
+                    PlayStopButton.Label = "Stop";
                 }
             }
 
+            // back/next/reset buttons change appearance depending on state of presentation
             ResetBackNextButtons();
         }
 
         private void NextButton_Click(object sender, RoutedEventArgs e)
         {
             int selectedIndex = PinnedNodesListView.SelectedIndex;
+
+            // can only move forward if there's a node to move forward to
             if (selectedIndex != PinnedNodesListView.Items.Count - 1)
             {
                 PinnedNodesListView.SelectedItem = PinnedNodesListView.Items[selectedIndex + 1];
@@ -75,22 +85,26 @@ namespace Dash.Views
             }
         }
 
+        // remove from viewmodel
         private void DeletePin(object sender, RoutedEventArgs e)
         {
             ViewModel.RemovePinFromPinnedNodesCollection((sender as Button).Tag as DocumentViewModel);
         }
 
+        // if we click a node, we should navigate to it immediately. Note that IsItemClickable is always enabled.
         private void PinnedNode_Click(object sender, ItemClickEventArgs e)
         {
             DocumentViewModel viewModel = (DocumentViewModel) e.ClickedItem;
             NavigateToDocument(viewModel);
         }
 
+        // helper method for moving the mainpage screen
         private void NavigateToDocument(DocumentViewModel viewModel)
         {
             MainPage.Instance.NavigateToDocumentInWorkspaceAnimated(viewModel.DocumentController);
         }
 
+        // these buttons are only enabled when the presentation is playing
         private void ResetBackNextButtons()
         {
             BackButton.IsEnabled = IsPresentationPlaying;
@@ -101,9 +115,37 @@ namespace Dash.Views
             ResetButton.Opacity = IsPresentationPlaying ? 1 : 0.3;
         }
 
+        // if user strays in middle of presentation, hitting this will bring them back to the selected node
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
             NavigateToDocument((DocumentViewModel) PinnedNodesListView.SelectedItem);
+        }
+
+        private void PinnedNodesListView_OnRightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            ListView listView = (ListView) sender;
+            PinnedNodeFlyout.ShowAt(listView, e.GetPosition(listView));
+            var source = (FrameworkElement) e.OriginalSource;
+            _textbox = source.GetFirstDescendantOfType<PresentationViewTextBox>() ?? source.GetFirstAncestorOfType<PresentationViewTextBox>();
+        }
+
+        private void Edit_OnClick(object sender, RoutedEventArgs e)
+        {
+            _giveTextBoxFocusUponFlyoutClosing = true;
+        }
+
+        private void Reset_OnClick(object sender, RoutedEventArgs e)
+        {
+            _textbox.ResetTitle();
+        }
+
+        private void Flyout_Closed(object sender, object e)
+        {
+            if (_giveTextBoxFocusUponFlyoutClosing)
+            {
+                _textbox.TriggerEdit();
+                _giveTextBoxFocusUponFlyoutClosing = false;
+            }
         }
     }
 }
