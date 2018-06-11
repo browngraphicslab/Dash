99382c55d6a7f73b22de63eb855d93771a432f60
madelinegr <mgriswold99@gmail.com>
2018-06-11 19:03:33 -0400

    we authenticated!

--- ChromeExt/popup.js
+++ ChromeExt/popup.js
@@ -23,14 +23,63 @@ document.addEventListener('DOMContentLoaded', function () {
             let urlSections = tab.url.split("/");
             let fileId = urlSections[urlSections.length - 2];
 
+            chrome.identity.getAuthToken({ 'interactive': true }, function (token) {
+                
+                console.log(token);
+                if (token) {
+                    document.body.style.backgroundColor = "blue";
+                    this.accessToken = token;
+                   // opt_callback && opt_callback();
+
+                    var xhr = new XMLHttpRequest();
+
+                    xhr.open("GET", "https://www.googleapis.com/drive/v3/files/" + fileId, true);
+                    xhr.setRequestHeader("Authorization", "Bearer " + token);
+                    xhr.onload = function () {
+                        console.log(xhr);
+                    }
+                    xhr.send('alt=media');
+                }
+                
+            });
+    /*
+            try {
+                chrome.identity.getAuthToken({ interactive: interactive }, function (token) {
+                    if (token) {
+                        this.accessToken = token;
+                        opt_callback && opt_callback();
+                    }
+                }.bind(this));
+            } catch (e) {
+                console.log(e);
+            }*/
+
+
+
+
             //I created bundle.js using browserfy to get require to work
-            const fs = require('fs');
-            document.body.style.backgroundColor = "blue";
+        //    const fs = require('fs');
             
-            // var accessToken = gapi.auth.getToken().access_token;
-            console.log(gapi.auth);
+            /*
+            gapi.client.init({
+                'apiKey': 'AIzaSyAI8fUZnDRr7zGh1vrHhngRRojlhrIQuW8',
+                'clientId': '533488754428-s7cp12aob9b9b11ll1l0ojtf82hnimsq.apps.googleusercontent.com',
+                'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+                'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest']
+            }).then(function () {
+                GoogleAuth = gapi.auth2.getAuthInstance();
+                
+                console.log(gapi.auth2);
+
+                GoogleAuth.signIn();
+
+                // Listen for sign-in state changes.
+                GoogleAuth.isSignedIn.listen(updateSigninStatus);
+            }); 
+             var accessToken = gapi.auth.getToken().access_token;
+            console.log(gapi.auth2);
             //I made this acess token up, won't work 
-            let accessToken = "a762t"
+             accessToken = "a762t"
             var xhr = new XMLHttpRequest();
            
             xhr.open("GET", "https://www.googleapis.com/drive/v3/files/" + fileId, true);
@@ -38,7 +87,7 @@ document.addEventListener('DOMContentLoaded', function () {
             xhr.onload = function () {
                 console.log(xhr);
             }
-            xhr.send('alt=media');
+            xhr.send('alt=media'); */
 
            /*
             let dest = fs.createWriteStream('Desktop/resume.pdf');
