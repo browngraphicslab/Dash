134113c81219c687203e6cb12d6f71e1d582f6ab
madelinegr <mgriswold99@gmail.com>
2018-06-11 09:27:43 -0400

    extention manifest

--- ChromeExt/keylistener.js
+++ ChromeExt/keylistener.js
@@ -1,4 +1,8 @@
-window.onkeyup = function(e) {
+document.body.style.backgroundColor = "yellow";
+var button = document.createElement("button");
+document.body.appendChild(button);
+
+window.onkeyup = function (e) {
    var key = e.keyCode ? e.keyCode : e.which;
       if (event.keyCode == 78) {
             chrome.extension.sendMessage({msg: "activate"}, function(response) {
