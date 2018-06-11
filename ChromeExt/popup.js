aa51934ecbc67f80318d47eec379a7b418eb7098
madelinegr <mgriswold99@gmail.com>
2018-06-11 11:53:14 -0400

    some url parsing

--- ChromeExt/popup.js
+++ ChromeExt/popup.js
@@ -7,14 +7,37 @@ document.addEventListener('DOMContentLoaded', function () {
 
         chrome.tabs.getSelected(null, function (tab) {
             //tab.url can now be used to get url
-            document.getElementById("openDash").innerHTML =
-                "The full URL of this page is:<br>" + tab.url;
             var url = tab.url;
             if (url.includes("docs.google.com")) {
                 //google doc is open
-                document.body.style.backgroundColor = "red";
+                
                 document.getElementById("addDoc").style.visibility = "visible";
             }
         });
    // }, false);
+
+    var addDocButton = document.getElementById('addDoc');
+    addDocButton.addEventListener('click', function () {
+        chrome.tabs.getSelected(null, function (tab) {
+        document.body.style.backgroundColor = "red";
+            var urlSections = tab.url.split("/");
+            var fileId = urlSections[urlSections.length - 2];
+
+            const fs = cep_node.require('fs');
+
+        var dest = fs.createWriteStream('Desktop/resume.pdf');
+        drive.files.export({
+            fileId: fileId,
+            mimeType: 'application/pdf'
+        })
+            .on('end', function () {
+                console.log('Done');
+            })
+            .on('error', function (err) {
+                console.log('Error during download', err);
+            })
+                .pipe(dest);
+        });
+    }, false);
+
 }, false)
