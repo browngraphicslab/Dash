400c186f7f46dbd5a1e74dc3b9028c8cc8b90b99
Tyler Schicke <tyler_schicke@brown.edu>
2018-06-12 18:16:51 -0400

    Formatting

--- Views/Document Menu/Toolbar/CollectionSubtoolbar.xaml.cs
+++ Views/Document Menu/Toolbar/CollectionSubtoolbar.xaml.cs
@@ -18,19 +18,20 @@ using System.Collections.ObjectModel;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
-namespace Dash {
+namespace Dash
+{
 
-	/// <summary>
-	/// The subtoolbar that is activated when a collection is selected on click. Implements ICommandBarBased because it uses a CommandBar as the UI.
-	/// </summary>
-	public sealed partial class CollectionSubtoolbar : UserControl, ICommandBarBased
+    /// <summary>
+    /// The subtoolbar that is activated when a collection is selected on click. Implements ICommandBarBased because it uses a CommandBar as the UI.
+    /// </summary>
+    public sealed partial class CollectionSubtoolbar : UserControl, ICommandBarBased
     {
         public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register(
             "Orientation", typeof(Orientation), typeof(CollectionSubtoolbar), new PropertyMetadata(default(Orientation)));
-		
+
         public Orientation Orientation
         {
-            get { return (Orientation) GetValue(OrientationProperty); }
+            get { return (Orientation)GetValue(OrientationProperty); }
             set { SetValue(OrientationProperty, value); }
         }
 
@@ -39,7 +40,7 @@ namespace Dash {
          */
         public void SetComboBoxVisibility(Visibility visibility) => xViewModesDropdown.Visibility = visibility;
 
-		private CollectionView _collection;
+        private CollectionView _collection;
 
         public CollectionSubtoolbar()
         {
@@ -59,98 +60,98 @@ namespace Dash {
             };
         }
 
-		/// <summary>
-		/// Formats the combo box according to Toolbar Constants.
-		/// </summary>
-		private void FormatDropdownMenu()
+        /// <summary>
+        /// Formats the combo box according to Toolbar Constants.
+        /// </summary>
+        private void FormatDropdownMenu()
         {
             xViewModesDropdown.Width = ToolbarConstants.ComboBoxWidth;
             xViewModesDropdown.Height = ToolbarConstants.ComboBoxHeight;
             xViewModesDropdown.Margin = new Thickness(ToolbarConstants.ComboBoxMarginOpen);
         }
 
-		/// <summary>
-		/// When the Break button is clicked, the selected group should separate.
-		/// </summary>
-		private void BreakGroup_OnClick(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// When the Break button is clicked, the selected group should separate.
+        /// </summary>
+        private void BreakGroup_OnClick(object sender, RoutedEventArgs e)
         {
             //TODO: Dismantle current selection (which must be a collection if the collection bar is showing)
             Debug.WriteLine("COLLECTION DISMANTLED/BROKEN!");
             xCollectionCommandbar.IsOpen = true;
             xCollectionCommandbar.IsEnabled = true;
 
-			//get list of doc views in the collection
-			var mainPageCollectionView =
-						   MainPage.Instance.MainDocView.GetFirstDescendantOfType<CollectionView>();
-			ObservableCollection<DocumentViewModel> vms = _collection.ViewModel.DocumentViewModels;
-			
-			//add them each to the main canvas
-			foreach ( DocumentViewModel vm in vms)
-			{
-				mainPageCollectionView.ViewModel.AddDocument(vm.DocumentController);
-			}
+            //get list of doc views in the collection
+            var mainPageCollectionView =
+                           MainPage.Instance.MainDocView.GetFirstDescendantOfType<CollectionView>();
+            ObservableCollection<DocumentViewModel> vms = _collection.ViewModel.DocumentViewModels;
 
-			//delete the sellected collection
-			var tempDocs = MainPage.Instance.GetSelectedDocuments().ToList<DocumentView>();
-			foreach (DocumentView d in tempDocs)
-			{
-				d.DeleteDocument();
-			}
-		}
+            //add them each to the main canvas
+            foreach (DocumentViewModel vm in vms)
+            {
+                mainPageCollectionView.ViewModel.AddDocument(vm.DocumentController);
+            }
 
-		/// <summary>
-		/// Binds the drop down selection of view otions with the view of the collection.
-		/// </summary>
-		private void ViewModesDropdown_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+            //delete the sellected collection
+            var tempDocs = MainPage.Instance.GetSelectedDocuments().ToList<DocumentView>();
+            foreach (DocumentView d in tempDocs)
+            {
+                d.DeleteDocument();
+            }
+        }
+
+        /// <summary>
+        /// Binds the drop down selection of view otions with the view of the collection.
+        /// </summary>
+        private void ViewModesDropdown_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             UpdateView();
         }
 
-		/// <summary>
-		/// Updates the view of the selected collection. 
-		/// </summary>
-		private void UpdateView()
+        /// <summary>
+        /// Updates the view of the selected collection. 
+        /// </summary>
+        private void UpdateView()
         {
-			if (_collection != null)
-			{
-				switch (xViewModesDropdown.SelectedIndex)
+            if (_collection != null)
             {
-				case 0:
-				_collection.SetView(CollectionView.CollectionViewType.Freeform);
-				break;
+                switch (xViewModesDropdown.SelectedIndex)
+                {
+                    case 0:
+                        _collection.SetView(CollectionView.CollectionViewType.Freeform);
+                        break;
 
-                case 1:
-				_collection.SetView(CollectionView.CollectionViewType.Grid);
-				break;
+                    case 1:
+                        _collection.SetView(CollectionView.CollectionViewType.Grid);
+                        break;
 
-                case 2:
-				_collection.SetView(CollectionView.CollectionViewType.Page);
-				break;
+                    case 2:
+                        _collection.SetView(CollectionView.CollectionViewType.Page);
+                        break;
 
-                case 3:
-				_collection.SetView(CollectionView.CollectionViewType.DB);
-				break;
+                    case 3:
+                        _collection.SetView(CollectionView.CollectionViewType.DB);
+                        break;
 
-                case 4:
-				_collection.SetView(CollectionView.CollectionViewType.Freeform);
-				break;
+                    case 4:
+                        _collection.SetView(CollectionView.CollectionViewType.Freeform);
+                        break;
 
-                case 5:
-				_collection.SetView(CollectionView.CollectionViewType.TreeView);
-				break;
+                    case 5:
+                        _collection.SetView(CollectionView.CollectionViewType.TreeView);
+                        break;
 
-                case 6:
-				_collection.SetView(CollectionView.CollectionViewType.Timeline);
-				break;
-			}
-                
+                    case 6:
+                        _collection.SetView(CollectionView.CollectionViewType.Timeline);
+                        break;
+                }
+
             }
         }
 
-		/// <summary>
-		/// Toggles the open/closed state of the command bar. 
-		/// </summary>
-		public void CommandBarOpen(bool status)
+        /// <summary>
+        /// Toggles the open/closed state of the command bar. 
+        /// </summary>
+        public void CommandBarOpen(bool status)
         {
             xCollectionCommandbar.IsOpen = status;
             xCollectionCommandbar.IsEnabled = true;
@@ -158,9 +159,9 @@ namespace Dash {
             xViewModesDropdown.Margin = status ? new Thickness(ToolbarConstants.ComboBoxMarginOpen) : new Thickness(ToolbarConstants.ComboBoxMarginClosed);
         }
 
-		public void SetCollectionBinding(CollectionView thisCollection)
-		{
-			_collection = thisCollection;
-		}
-	}
+        public void SetCollectionBinding(CollectionView thisCollection)
+        {
+            _collection = thisCollection;
+        }
+    }
 }
--- Views/Document Menu/Toolbar/ImageSubtoolbar.xaml.cs
+++ Views/Document Menu/Toolbar/ImageSubtoolbar.xaml.cs
@@ -21,117 +21,117 @@ using Dash.Views.Document_Menu.Toolbar;
 
 namespace Dash
 {
-	/// <summary>
-	/// The subtoolbar that appears when an ImageBox is selected. Implements ICommandBarBased because it created with a CommandBar.
-	/// </summary>
-	public sealed partial class ImageSubtoolbar : UserControl, ICommandBarBased
-	{
-	    public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register(
-	        "Orientation", typeof(Orientation), typeof(ImageSubtoolbar), new PropertyMetadata(default(Orientation)));
+    /// <summary>
+    /// The subtoolbar that appears when an ImageBox is selected. Implements ICommandBarBased because it created with a CommandBar.
+    /// </summary>
+    public sealed partial class ImageSubtoolbar : UserControl, ICommandBarBased
+    {
+        public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register(
+            "Orientation", typeof(Orientation), typeof(ImageSubtoolbar), new PropertyMetadata(default(Orientation)));
 
-		//orientation binding, currently inactive
-	    public Orientation Orientation
-	    {
-	        get { return (Orientation) GetValue(OrientationProperty); }
-	        set { SetValue(OrientationProperty, value); }
-	    }
+        //orientation binding, currently inactive
+        public Orientation Orientation
+        {
+            get { return (Orientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
 
-	    public void SetComboBoxVisibility(Visibility visibility) => xScaleOptionsDropdown.Visibility = visibility;
+        public void SetComboBoxVisibility(Visibility visibility) => xScaleOptionsDropdown.Visibility = visibility;
 
         private DocumentView currentDocView;
-	    private DocumentController currentDocController;
+        private DocumentController currentDocController;
 
-	
-		public ImageSubtoolbar()
+
+        public ImageSubtoolbar()
         {
-			this.InitializeComponent();
-		    FormatDropdownMenu();
+            this.InitializeComponent();
+            FormatDropdownMenu();
 
-			//binds orientation of the subtoolbar to the current orientation of the main toolbar (inactive functionality)
-		    xImageCommandbar.Loaded += delegate
-		    {
-		        var sp = xImageCommandbar.GetFirstDescendantOfType<StackPanel>();
-		        sp.SetBinding(StackPanel.OrientationProperty, new Binding
-		        {
-		            Source = this,
-		            Path = new PropertyPath(nameof(Orientation)),
-		            Mode = BindingMode.OneWay
-		        });
-		        Visibility = Visibility.Collapsed;
-		    };
-		}
+            //binds orientation of the subtoolbar to the current orientation of the main toolbar (inactive functionality)
+            xImageCommandbar.Loaded += delegate
+            {
+                var sp = xImageCommandbar.GetFirstDescendantOfType<StackPanel>();
+                sp.SetBinding(StackPanel.OrientationProperty, new Binding
+                {
+                    Source = this,
+                    Path = new PropertyPath(nameof(Orientation)),
+                    Mode = BindingMode.OneWay
+                });
+                Visibility = Visibility.Collapsed;
+            };
+        }
 
-		/// <summary>
-		/// Formats the combo box according to Toolbar Constants.
-		/// </summary>
-		private void FormatDropdownMenu()
+        /// <summary>
+        /// Formats the combo box according to Toolbar Constants.
+        /// </summary>
+        private void FormatDropdownMenu()
         {
             xScaleOptionsDropdown.Width = ToolbarConstants.ComboBoxWidth;
             xScaleOptionsDropdown.Height = ToolbarConstants.ComboBoxHeight;
             xScaleOptionsDropdown.Margin = new Thickness(ToolbarConstants.ComboBoxMarginOpen);
         }
 
-		/// <summary>
-		/// Prevents command bar from hiding labels on click by setting isOpen to true every time it begins to close.
-		/// </summary>
-		private void CommandBar_Closing(object sender, object e)
-		{
-			xImageCommandbar.IsOpen = true;
-		}
-
-	
-		private void Crop_Click(object sender, RoutedEventArgs e)
-		{
-            //TODO: Implement cropping on the selected image
-		    xImageCommandbar.IsOpen = true;
+        /// <summary>
+        /// Prevents command bar from hiding labels on click by setting isOpen to true every time it begins to close.
+        /// </summary>
+        private void CommandBar_Closing(object sender, object e)
+        {
+            xImageCommandbar.IsOpen = true;
         }
 
-		/// <summary>
-		/// Called when the Replace Button is clicked. Calls on helper method to replace the most recently selected image.
-		/// </summary>
-		private void Replace_Click(object sender, RoutedEventArgs e)
-		{
+
+        private void Crop_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: Implement cropping on the selected image
+            xImageCommandbar.IsOpen = true;
+        }
+
+        /// <summary>
+        /// Called when the Replace Button is clicked. Calls on helper method to replace the most recently selected image.
+        /// </summary>
+        private void Replace_Click(object sender, RoutedEventArgs e)
+        {
             ReplaceImage();
-		    xImageCommandbar.IsOpen = true;
-		}
+            xImageCommandbar.IsOpen = true;
+        }
 
-		/// <summary>
-		/// Toggles open/closed states of this subtoolbar.
-		/// </summary>
-		public void CommandBarOpen(bool status)
-	    {
-	        xImageCommandbar.IsOpen = status;
-	        xImageCommandbar.IsEnabled = true;
-	        xImageCommandbar.Visibility = Visibility.Visible;
-			//updates margin to visually account for the change in size
+        /// <summary>
+        /// Toggles open/closed states of this subtoolbar.
+        /// </summary>
+        public void CommandBarOpen(bool status)
+        {
+            xImageCommandbar.IsOpen = status;
+            xImageCommandbar.IsEnabled = true;
+            xImageCommandbar.Visibility = Visibility.Visible;
+            //updates margin to visually account for the change in size
             xScaleOptionsDropdown.Margin = status ? new Thickness(ToolbarConstants.ComboBoxMarginOpen) : new Thickness(ToolbarConstants.ComboBoxMarginClosed);
-	    }
+        }
 
-		/// <summary>
-		/// Helper method for replacing the selected image. Opens file picker and and sets the field of the image controller to the new image's URI.
-		/// </summary>
-		private async void ReplaceImage()
-	    {
-	        var imagePicker = new FileOpenPicker
-	        {
-	            ViewMode = PickerViewMode.Thumbnail,
-	            SuggestedStartLocation = PickerLocationId.PicturesLibrary
-	        };
-	        imagePicker.FileTypeFilter.Add(".jpg");
-	        imagePicker.FileTypeFilter.Add(".jpeg");
-	        imagePicker.FileTypeFilter.Add(".bmp");
-	        imagePicker.FileTypeFilter.Add(".png");
-	        imagePicker.FileTypeFilter.Add(".svg");
+        /// <summary>
+        /// Helper method for replacing the selected image. Opens file picker and and sets the field of the image controller to the new image's URI.
+        /// </summary>
+        private async void ReplaceImage()
+        {
+            var imagePicker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = PickerLocationId.PicturesLibrary
+            };
+            imagePicker.FileTypeFilter.Add(".jpg");
+            imagePicker.FileTypeFilter.Add(".jpeg");
+            imagePicker.FileTypeFilter.Add(".bmp");
+            imagePicker.FileTypeFilter.Add(".png");
+            imagePicker.FileTypeFilter.Add(".svg");
 
-			//update image controller's URI to the new image's URI
-	        var replacement = await imagePicker.PickSingleFileAsync();
-	        if (replacement != null) { currentDocController.SetField<ImageController>(KeyStore.DataKey, await ImageToDashUtil.GetLocalURI(replacement), true); }
-	    }
+            //update image controller's URI to the new image's URI
+            var replacement = await imagePicker.PickSingleFileAsync();
+            if (replacement != null) { currentDocController.SetField<ImageController>(KeyStore.DataKey, await ImageToDashUtil.GetLocalURI(replacement), true); }
+        }
 
-		/// <summary>
-		/// Enables the subtoolbar access to the Document View of the image that was selected on tap.
-		/// </summary>
-		internal void SetImageBinding(DocumentView selection)
+        /// <summary>
+        /// Enables the subtoolbar access to the Document View of the image that was selected on tap.
+        /// </summary>
+        internal void SetImageBinding(DocumentView selection)
         {
             currentDocView = selection;
             currentDocController = currentDocView.ViewModel.DocumentController;
