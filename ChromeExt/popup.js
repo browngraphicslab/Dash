8973f979b38e72d0d71c83d634107075df7f3584
madelinegr <mgriswold99@gmail.com>
2018-06-12 16:51:37 -0400

    pdf downloaded!

--- ChromeExt/popup.js
+++ ChromeExt/popup.js
@@ -29,29 +29,56 @@ document.addEventListener('DOMContentLoaded', function () {
                    // xhr.mimeType = "application/vnd.google-apps.document";
 
                     //the second param is specific to doc
-                    xhr.open("GET", "https://www.googleapis.com/drive/v3/files/" + fileId + "/export?alt=media&mimeType=text%plain", true);
+                    xhr.open("GET", "https://www.googleapis.com/drive/v3/files/" + fileId + "/export?mimeType=application%2Fpdf", true);
                     xhr.setRequestHeader("Authorization", "Bearer " + token);
+                    xhr.responseType = "arraybuffer"
 
-   
-                    xhr.overrideMimeType('text/plain; charset=x-user-defined');
+ 
                    // xhr.responseType = 'blob';
                     xhr.onload = function () {
                         console.log(xhr);
+                        var pdf = xhr.response;
+
+                        var array = new Uint8Array(pdf);
+
+                        var text = array,
+                            blob = new Blob([text], { type: 'octet/stream' }),
+                            anchor = document.createElement('a');
+
+                        anchor.download = "hello.pdf";
+                        anchor.href = (window.webkitURL || window.URL).createObjectURL(blob);
+                        //anchor.dataset.downloadurl = ['application/pdf', anchor.download, anchor.href].join(':');
+                        anchor.click();
+
+
+
+                        /*
+                        var bb = new BlobBuilder();
+                        bb.append((new XMLSerializer).serializeToString(pdf));
+                        var blob = bb.getBlob("application/xhtml+xml;charset=");
+                        saveAs(blob, "document.xhtml");
+                        
+                        var doc = new jsPDF();
+                        doc.text(pdf);
+                        doc.save('docPdfMade.pdf');
+
+                        
                         var reader = new FileReader();
                         reader.onload = function () {
 
                             console.log(reader.result); // dataURI
 
                         }
-                        reader.readAsDataURL(this.response);
+                        reader.readAsDataURL(this.response);*/
                        // console.log(xhr.responseText);
                     }
+                    /*
                     xhr.onreadystatechange = function () {
                         console.log(xhr);
                         if (xhr.readyState == 4) {
                             save(xhr.response);
                         }
-                    }
+                    }*/
                     xhr.send('alt=media');
                 }
                 
@@ -120,55 +147,3 @@ document.addEventListener('DOMContentLoaded', function () {
     }, false);
 
 }, false)
-
-function base64ArrayBuffer(arrayBuffer) {
-    var base64 = ''
-    var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-
-    var bytes = new Uint8Array(arrayBuffer)
-    var byteLength = bytes.byteLength
-    var byteRemainder = byteLength % 3
-    var mainLength = byteLength - byteRemainder
-
-    var a, b, c, d
-    var chunk
-
-    // Main loop deals with bytes in chunks of 3
-    for (var i = 0; i < mainLength; i = i + 3) {
-        // Combine the three bytes into a single integer
-        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
-
-        // Use bitmasks to extract 6-bit segments from the triplet
-        a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
-        b = (chunk & 258048) >> 12 // 258048   = (2^6 - 1) << 12
-        c = (chunk & 4032) >> 6 // 4032     = (2^6 - 1) << 6
-        d = chunk & 63               // 63       = 2^6 - 1
-
-        // Convert the raw binary segments to the appropriate ASCII encoding
-        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
-    }
-
-    // Deal with the remaining bytes and padding
-    if (byteRemainder == 1) {
-        chunk = bytes[mainLength]
-
-        a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
-
-        // Set the 4 least significant bits to zero
-        b = (chunk & 3) << 4 // 3   = 2^2 - 1
-
-        base64 += encodings[a] + encodings[b] + '=='
-    } else if (byteRemainder == 2) {
-        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
-
-        a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
-        b = (chunk & 1008) >> 4 // 1008  = (2^6 - 1) << 4
-
-        // Set the 2 least significant bits to zero
-        c = (chunk & 15) << 2 // 15    = 2^4 - 1
-
-        base64 += encodings[a] + encodings[b] + encodings[c] + '='
-    }
-
-    return base64
-}
