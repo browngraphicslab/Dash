e647c46b0f3f51a7cb96e473aa1e500fbc577d75
Stanley Yip <stanley_yip@brown.edu>
2018-06-18 14:39:18 -0400

    give tanky boi one big restart boi

---
+++ Views/Collection/GraphConnection.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using Dash.Annotations;
+
+namespace Dash
+{
+    public class GraphConnection : INotifyPropertyChanged
+    {
+        private GraphNodeView _fromDoc;
+        private GraphNodeView _toDoc;
+        private Point _toPoint;
+        private Point _fromPoint;
+
+        public Point ToPoint
+        {
+            get => _toPoint;
+            set => _toPoint = value;
+        }
+
+        public Point FromPoint
+        {
+            get => _fromPoint;
+            set => _fromPoint = value;
+        }
+
+        public GraphNodeView FromDoc
+        {
+            get => _fromDoc;
+            set
+            {
+                _fromDoc = value;
+                OnPropertyChanged(nameof(FromDoc));
+                FromDoc.ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+            }
+        }
+        public GraphNodeView ToDoc
+        {
+            get => _toDoc;
+            set
+            {
+                _toDoc = value;
+                OnPropertyChanged(nameof(ToDoc));
+                ToDoc.ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+            }
+        }
+
+        public Polyline Connection { get; }
+
+        public GraphConnection()
+        {
+            Connection = new Polyline
+            {
+                Stroke = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0)),
+                StrokeThickness = 2
+            };
+            _fromPoint = new Point();
+            _toPoint = new Point();
+            PropertyChanged += GraphConnection_PropertyChanged;
+        }
+
+        private void GraphConnection_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(ToDoc):
+                case nameof(FromDoc):
+                    UpdateConnection();
+                    break;
+            }
+        }
+
+        private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals(nameof(ToDoc.ViewModel.XPosition)) ||
+                e.PropertyName.Equals(nameof(ToDoc.ViewModel.YPosition)))
+            {
+                UpdateConnection();
+            }
+        }
+
+        private void UpdateConnection()
+        {
+            if (ToDoc != null && FromDoc != null)
+            {
+                Connection.Points.Clear();
+                _toPoint.X = ToDoc.ViewModel.XPosition + ToDoc.xEllipse.Width / 2;
+                _toPoint.Y = ToDoc.ViewModel.YPosition + ToDoc.xEllipse.Height / 2;
+                _fromPoint.X = FromDoc.ViewModel.XPosition + FromDoc.xEllipse.Width / 2;
+                _fromPoint.Y = FromDoc.ViewModel.YPosition + FromDoc.xEllipse.Height / 2;
+                Connection.Points.Add(_toPoint);
+                Connection.Points.Add(_fromPoint);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Views/Collection/NodeConnectionsView.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Dash.Annotations;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Dash
+{
+    public sealed partial class NodeConnectionsView : UserControl, INotifyPropertyChanged
+    {
+        public DocumentViewModel ViewModel { get; set; }
+        public CollectionGraphView ParentGraph { get; private set; }
+        public double ConstantRadiusWidth { get; set; }
+        public ObservableCollection<ListViewItem> ToConnections { get; set; }
+        public ObservableCollection<ListViewItem> FromConnections { get; set; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+
+        public NodeConnectionsView(DocumentViewModel viewmodel, CollectionGraphView parent)
+        {
+            ToConnections = new ObservableCollection<ListViewItem>();
+            ToConnections.Add(new ListViewItem { Content = "Linked To:", FontWeight = FontWeights.Bold });
+            FromConnections = new ObservableCollection<ListViewItem>();
+            FromConnections.Add(new ListViewItem { Content = "Linked From:", FontWeight = FontWeights.Bold });
+            ViewModel = viewmodel;
+            ParentGraph = parent;
+            InitializeComponent();
+            
+            Loaded += GraphInfoView_Loaded;
+            Unloaded += GraphInfoView_Unloaded;
+        }
+
+        private void GraphInfoView_Unloaded(object sender, RoutedEventArgs e)
+        {
+            ViewModel.PropertyChanged -= ViewModel_PropertyChanged;
+        }
+
+        private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+           
+        }
+
+
+        #region loading
+
+        private void GraphInfoView_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+            ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+            CreateInfo();
+        }
+
+   
+        private void UpdateData()
+        {
+
+            
+        }
+
+        private void CreateInfo()
+        {
+            var toLinks = ParentGraph.AdjacencyLists[ViewModel];
+            foreach (var doc in toLinks)
+            {
+                var lvi = new ListViewItem
+                {
+                    Content =
+                        doc.DocumentController.GetDereferencedField<TextController>(KeyStore.TitleKey, null).Data,
+                    DataContext = doc
+                };
+                lvi.Tapped += ListItem_Tapped;
+                ToConnections.Add(lvi);
+            }
+
+            var allLinks = ParentGraph.Connections;
+            foreach (var link in allLinks)
+            {
+                if (link.Value.Equals(ViewModel))
+                {
+                    var lvi = new ListViewItem
+                    {
+                        Content =
+                            link.Key.DocumentController.GetDereferencedField<TextController>(KeyStore.TitleKey, null).Data,
+                        DataContext = link.Key
+                    };
+                    lvi.Tapped += ListItem_Tapped;
+                    FromConnections.Add(lvi);
+                }
+            }
+
+            if (ToConnections.Count <= 1)
+            {
+                xLinkToDocs.Visibility = Visibility.Collapsed;
+            }
+
+            if (FromConnections.Count <= 1)
+            {
+                xLinkFromDocs.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void ListItem_Tapped(object sender, TappedRoutedEventArgs tappedRoutedEventArgs)
+        {
+            if ((sender as ListViewItem)?.DataContext is DocumentViewModel dvm)
+            {
+                var gnv = ParentGraph.CollectionCanvas.First(i => i.ViewModel.DocumentViewModel.Equals(dvm));
+                gnv.NavigateTo();
+            }
+        }
+
+        #endregion
+
+
+        #region property changed
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+        
+    }
+}
