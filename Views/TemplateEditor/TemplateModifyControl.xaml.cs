093ee11997bb635d4fb4a57a6901ebabd22f5b0e
hannah <hannah_chow@brown.edu>
2018-06-21 13:50:44 -0400

    beginning modify control

--- Views/TemplateEditor/TemplateModifyControl.xaml.cs
+++ Views/TemplateEditor/TemplateModifyControl.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
-namespace Dash.Views.TemplateEditor
+namespace Dash
 {
     public sealed partial class TemplateModifyControl : UserControl
     {
