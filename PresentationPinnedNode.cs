5c9d4067577b53171f68a74782feeb88ddc00f07
Crystal Chen <hui_chen@brown.edu>
2018-06-27 16:24:18 -0400

    no message

--- PresentationPinnedNode.cs
+++ PresentationPinnedNode.cs
@@ -8,6 +8,9 @@ namespace Dash
 {
     public class PresentationPinnedNode
     {
+        /// <summary>
+        /// Key value pair of DocumentViewModel and the scale of its parent collection at the time it was pinned to the presentation
+        /// </summary>
         public KeyValuePair<DocumentViewModel, double> Data { get; set; }
 
         public PresentationPinnedNode(DocumentViewModel viewModel, double scale)
