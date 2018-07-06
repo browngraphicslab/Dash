4cfa14aacdfbb8e99532f0c4414a5220ce1ad864
madelinegr <mgriswold99@gmail.com>
2018-07-06 09:52:15 -0400

    return scope has no parent, no varaible conflicts

--- Util/DSL/FunctionDeclarationExpression.cs
+++ Util/DSL/FunctionDeclarationExpression.cs
@@ -28,8 +28,6 @@ namespace Dash
         {
             var functionOperator = new FunctionOperatorController(_parameters, _funcBlock, _returnType);
 
-            //scope?.DeclareVariable(_funcName, functionOperator);
-
             return functionOperator;
         }
 
