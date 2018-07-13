e365018202fb0a93329e6d8350dfd5984051c1ea
Brandon <brandon_li@brown.edu>
2018-07-13 11:06:10 -0400

    test

--- Controllers/Operators/search/SearchResult.cs
+++ Controllers/Operators/search/SearchResult.cs
@@ -12,14 +12,14 @@ namespace Dash
 
         public SearchResult() : this(null, "", "", 0) { }
 
-        public SearchResult(FieldControllerBase doc)
-        {
-            ViewDocument = doc as DocumentController;
-            DataDocument = doc as DocumentController;
-            TitleAppendix = "";
-            RelevantText = "";
-            Rank = 1;
-        }
+        //public SearchResult(FieldControllerBase doc)
+        //{
+        //    ViewDocument = doc as DocumentController;
+        //    DataDocument = doc as DocumentController;
+        //    TitleAppendix = "";
+        //    RelevantText = "";
+        //    Rank = 1;
+        //}
 
         public SearchResult(DocumentNode node, string titleAppendix, string relevantText, int rank = 1)
         {
