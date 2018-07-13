6f142183df514517fc36aef0ca629513cee6f809
bob <bcz@cs.brown.edu>
2018-07-13 16:08:45 -0400

    added subsnapshots and _copy ... needs work.

--- CollectionFreeformBase.cs
+++ CollectionFreeformBase.cs
@@ -150,6 +150,7 @@ namespace Dash
             foreach (var dvm in ViewModel.DocumentViewModels)
                 controllers.Add(copyData ? dvm.DocumentController.GetDataCopy() : dvm.DocumentController.GetViewCopy());
             var snap = new CollectionNote(new Point(), CollectionView.CollectionViewType.Freeform, double.NaN, double.NaN, controllers).Document;
+            snap.GetDataDocument().SetTitle(ParentDocument.ViewModel.DocumentController.Title + "_copy");
             snap.SetFitToParent(true);
             return snap;
         }
