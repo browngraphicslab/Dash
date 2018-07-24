87450aabadd7e248a6e50c2b5cb93dd9756a051c
madelinegr <mgriswold99@gmail.com>
2018-07-24 15:08:14 -0400

    better code font

--- CollectionFreeformBase.cs
+++ CollectionFreeformBase.cs
@@ -975,7 +975,14 @@ namespace Dash
 
         void MakeInkCanvas()
         {
-            XInkCanvas = new InkCanvas() { Width = 60000, Height = 60000, Visibility = Visibility.Collapsed};
+            try
+            {
+                XInkCanvas = new InkCanvas() {Width = 60000, Height = 60000, Visibility = Visibility.Collapsed};
+            }
+            catch (System.OutOfMemoryException)
+            {
+                Debug.WriteLine("Out of memory!");
+            }
 
             InkControl = new FreeformInkControl(this, XInkCanvas, SelectionCanvas);
             Canvas.SetLeft(XInkCanvas, -30000);
