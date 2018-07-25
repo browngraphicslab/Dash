0ef972b285cc449c13d51658f52867f11d11f366
Laura Wilson <laura_wilson@brown.edu>
2018-07-25 16:10:11 -0400

    text subtoolbar selection issues fixed

--- Util/DashColorPicker.xaml.cs
+++ Util/DashColorPicker.xaml.cs
@@ -66,7 +66,7 @@ namespace Dash
 
             Unloaded += (sender, args) =>
             {
-                SelectedColor = LastDiscreteSelection;
+                //SelectedColor = LastDiscreteSelection;
                 xSavedColorsStack.Children.Clear();
             };
         }
