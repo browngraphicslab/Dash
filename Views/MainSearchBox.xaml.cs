a3dd33b67412da86e5829983d3da6bd262aba84e
Brandon <brandon_li@brown.edu>
2018-07-29 19:44:31 -0400

    Fixed small bug where Dash crashes with links in graph view

--- Views/MainSearchBox.xaml.cs
+++ Views/MainSearchBox.xaml.cs
@@ -298,6 +298,8 @@ namespace Dash
             if (searchBox == null) return;
 
             UnHighlightAllDocs();
+
+            var a = DocumentTree.MainPageTree;
             
             //TODO This is going to screw up regex by making it impossible to specify regex with capital letters
             string text = searchBox.Text; //.ToLower();
