980bf5860d5b95b8bb5b62c273381a3e809cc023
madelinegr <mgriswold99@gmail.com>
2018-08-20 20:54:00 -0400

    float link description when link followed

--- AnnotationManager.cs
+++ AnnotationManager.cs
@@ -128,17 +128,11 @@ namespace Dash
 	    private void FollowLink(DocumentController link, LinkDirection direction, IEnumerable<ILinkHandler> linkHandlers)
 	    {
             //show link description floating doc if operator output is true
-	        var linkOperator = link.GetDataDocument().GetField<ListController<OperatorController>>(KeyStore.OperatorKey);
-
-	        var lastLink = linkOperator.Last();
-
-	        var inputs = lastLink.Outputs.Values;
-	        MainPage.Instance.AddFloatingDoc(link);
-
-
-
-
-	        
+	        var linkOperator = link.GetDataDocument().GetDereferencedField<BoolController>(LinkDescriptionTextOperator.ShowDescription, null);
+	        if (linkOperator.Data)
+	        {
+	            MainPage.Instance.AddFloatingDoc(link);
+            }
 
 	        foreach (ILinkHandler linkHandler in linkHandlers)
 	        {
