2e02d7192f901d9335c0fc987afe92c9b5c6cedd
bobzel <zzzman@gmail.com>
2018-08-30 01:19:50 -0400

    fixed making region selections on images.

--- ViewModels/CollectionViewModel.cs
+++ ViewModels/CollectionViewModel.cs
@@ -734,12 +734,12 @@ namespace Dash
                     {
                         for (var i = 0; i < d.SourceCollectionViews?.Count; i++)
                         {
-                            if (d.SourceCollectionViews[i].ViewModel == this)
+                            if (d.SourceCollectionViews[i]?.ViewModel == this)
                             {
                                 docsToAdd.Remove(d.DraggedDocuments[i]);
                                 continue;
                             }
-                            d.SourceCollectionViews[i].ViewModel.RemoveDocument(d.DraggedDocuments[i]);
+                            d.SourceCollectionViews[i]?.ViewModel.RemoveDocument(d.DraggedDocuments[i]);
                         }
                     }
                 }
