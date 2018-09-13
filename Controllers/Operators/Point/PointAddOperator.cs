0480409f9d3826dc8f0de765d3587c427b6c836b
Tyler Schicke <tyler_schicke@brown.edu>
2018-09-13 02:52:41 -0400

    Added some point operators

--- Controllers/Operators/Point/PointAddOperator.cs
+++ Controllers/Operators/Point/PointAddOperator.cs
@@ -1,50 +1,58 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using DashShared;
 
-namespace Dash
+namespace Dash.Controllers.Operators.Point
 {
-    [OperatorType(Op.Name.add, Op.Name.concat, Op.Name.operator_add)]
-    public sealed class ConcatOperatorController : OperatorController
+    [OperatorType(Op.Name.operator_add)]
+    public class PointAddOperator : OperatorController
     {
         public static readonly KeyController AKey = new KeyController("A");
         public static readonly KeyController BKey = new KeyController("B");
 
+
         public static readonly KeyController OutputKey = new KeyController("Output");
 
 
-        public ConcatOperatorController() : base(new OperatorModel(TypeKey.KeyModel)) => SaveOnServer();
+        public PointAddOperator() : base(new OperatorModel(TypeKey.KeyModel))
+        {
+            SaveOnServer();
+        }
 
-        public ConcatOperatorController(OperatorModel operatorFieldModel) : base(operatorFieldModel)
+        public PointAddOperator(OperatorModel operatorFieldModel) : base(operatorFieldModel)
         {
         }
 
         public override KeyController OperatorType { get; } = TypeKey;
-        private static readonly KeyController TypeKey = new KeyController("Concat", "F69DF9CF-5B51-482D-AE1E-40B3266930CB");
+        private static readonly KeyController TypeKey = new KeyController("PointAdd", "59e828b8-0e30-4bd3-904b-9919f89cc4be");
 
         public override FieldControllerBase GetDefaultController()
         {
-            return new ConcatOperatorController();
+            return new PointAddOperator();
         }
 
         public override ObservableCollection<KeyValuePair<KeyController, IOInfo>> Inputs { get; } = new ObservableCollection<KeyValuePair<KeyController, IOInfo>>
         {
-            new KeyValuePair<KeyController, IOInfo>(AKey, new IOInfo(TypeInfo.Text, true)),
-            new KeyValuePair<KeyController, IOInfo>(BKey, new IOInfo(TypeInfo.Text, true)),
+            new KeyValuePair<KeyController, IOInfo>(AKey, new IOInfo(TypeInfo.Point, true)),
+            new KeyValuePair<KeyController, IOInfo>(BKey, new IOInfo(TypeInfo.Point, true)),
+
         };
 
         public override ObservableDictionary<KeyController, TypeInfo> Outputs { get; } = new ObservableDictionary<KeyController, TypeInfo>
         {
-            [OutputKey] = TypeInfo.Text
+            [OutputKey] = TypeInfo.Point,
+
         };
 
         public override void Execute(Dictionary<KeyController, FieldControllerBase> inputs,
             Dictionary<KeyController, FieldControllerBase> outputs,
             DocumentController.DocumentFieldUpdatedEventArgs args, Scope scope = null)
         {
-            var a = (inputs[AKey] as TextController)?.Data;
-            var b = (inputs[BKey] as TextController)?.Data;
-            outputs[OutputKey] = new TextController(a + b);
+            var a = ((PointController)inputs[AKey]).Data;
+            var b = ((PointController)inputs[BKey]).Data;
+            outputs[OutputKey] = new PointController(a.X + b.X, a.Y + b.Y);
         }
+
     }
 }
---
+++ Controllers/Operators/Point/PointOperator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DashShared;
+
+namespace Dash
+{
+    public sealed class PointOperator : OperatorController
+    {
+
+        public static readonly KeyController XKey = new KeyController("X");
+        public static readonly KeyController YKey = new KeyController("Y");
+
+        public static readonly KeyController PointKey = new KeyController("Point");
+
+        public PointOperator() : base(new OperatorModel(TypeKey.KeyModel))
+        {
+            SaveOnServer();
+        }
+
+        public PointOperator(OperatorModel operatorFieldModel) : base(operatorFieldModel)
+        {
+        }
+
+        public override FieldControllerBase GetDefaultController()
+        {
+            return new PointOperator();
+        }
+
+        public override ObservableCollection<KeyValuePair<KeyController, IOInfo>> Inputs { get; } = new ObservableCollection<KeyValuePair<KeyController, IOInfo>>
+        {
+            new KeyValuePair<KeyController, IOInfo>(XKey, new IOInfo(TypeInfo.Number, true)),
+            new KeyValuePair<KeyController, IOInfo>(YKey, new IOInfo(TypeInfo.Number, true))
+        };
+
+        public override ObservableDictionary<KeyController, TypeInfo> Outputs { get; } = new ObservableDictionary<KeyController, TypeInfo>
+        {
+            [PointKey] = TypeInfo.Point
+        };
+
+        public override KeyController OperatorType { get; } = TypeKey;
+        private static readonly KeyController TypeKey = new KeyController("Point", "45C9F1AB-1E61-453E-B3DB-A17A81A2C428");
+
+        public override void Execute(Dictionary<KeyController, FieldControllerBase> inputs, Dictionary<KeyController, FieldControllerBase> outputs, DocumentController.DocumentFieldUpdatedEventArgs args, Scope scope = null)
+        {
+            var x = ((NumberController) inputs[XKey]).Data;
+            var y = ((NumberController) inputs[YKey]).Data;
+            outputs[PointKey] = new PointController(x, y);
+        }
+    }
+}
