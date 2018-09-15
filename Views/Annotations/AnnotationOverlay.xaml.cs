a1434234bc947cb6c49dd3feb48bb0b643e49953
Stanley Yip <stanley_yip@brown.edu>
2018-09-15 16:11:49 -0400

    im gonna explode

--- Views/Annotations/AnnotationOverlay.xaml.cs
+++ Views/Annotations/AnnotationOverlay.xaml.cs
@@ -720,9 +720,14 @@ namespace Dash
                     var found = false;
                     foreach (var rect in _clipRectSelections)
                     {
-                        var ahhh = ele.Bounds.Left < Canvas.GetLeft(rect) + rect.Width + ele.Bounds.Width && Canvas.GetLeft(rect) < ele.Bounds.Left && ele.Bounds.Left - rect.Width < Canvas.GetLeft(rect) + ele.Bounds.Width;
-                        var fromRight = Canvas.GetLeft(rect) - ele.Bounds.Left < ele.Bounds.Width * 2 && Canvas.GetLeft(rect) > ele.Bounds.Left && Canvas.GetLeft(rect) - ele.Bounds.Right < ele.Bounds.Width;
-                        var closeEnough = (ahhh || fromRight) &&
+                        if (found || _selectedRectangles.ContainsKey(ele.Index)) break;
+                        //var ahhh = ele.Bounds.Left < Canvas.GetLeft(rect) + rect.Width + ele.Bounds.Width && Canvas.GetLeft(rect) < ele.Bounds.Left && ele.Bounds.Left - rect.Width < Canvas.GetLeft(rect) + ele.Bounds.Width;
+                        //var fromRight = Canvas.GetLeft(rect) - ele.Bounds.Left < ele.Bounds.Width * 2 && Canvas.GetLeft(rect) > ele.Bounds.Left && Canvas.GetLeft(rect) - ele.Bounds.Right < ele.Bounds.Width;
+                        //var closeEnough = (ahhh || fromRight) &&
+                        //                  Math.Abs(ele.Bounds.Top - Canvas.GetTop(rect)) <
+                        //                  ele.Bounds.Height / 2;
+                        var closeEnough = Math.Abs(ele.Bounds.Left - Canvas.GetLeft(rect)) <
+                                          ele.Bounds.Width + rect.Width &&
                                           Math.Abs(ele.Bounds.Top - Canvas.GetTop(rect)) <
                                           ele.Bounds.Height / 2;
                         var similarSize = ele.Bounds.Height - rect.Height < ele.Bounds.Height;
@@ -737,7 +742,7 @@ namespace Dash
                         }
                         else if (/*rect.IsInVisualTree() &&*/ new Rect(Canvas.GetLeft(rect), Canvas.GetTop(rect), rect.Width, rect.Height).Contains(ele.Bounds))
                         {
-                            found = true; 
+                            found = true;
                             _selectedRectangles[ele.Index] = rect;
                         }
                     }
