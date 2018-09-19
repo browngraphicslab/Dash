b4e7d5206f583d83ccb11b073888c5ff47579de7
julie <juliewang42@gmail.com>
2018-09-18 20:32:27 -0400

    skeleton structure

---
+++ Util/CourtesyDocuments/TableBox.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Dash.Converters;
+using DashShared;
+
+namespace Dash
+{
+    class TableBox : CourtesyDocument
+    {
+        public static DocumentType DocumentType = new DocumentType("45D372AB-C25A-4EC4-87DF-D63257CFFFE0", "Table Box");
+        public static readonly string PrototypeId = "FB270B9A-FFFA-41B8-8A60-94D86EC87B2Fs";
+
+        public TableBox(FieldControllerBase refToData, double x = 0, double y = 0, double w = double.NaN,
+            double h = double.NaN)
+        {
+            var fields = DefaultLayoutFields(new Point(x, y), new Size(w, h), refToData);
+            SetupDocument(DocumentType, PrototypeId, "Table Box Prototype Layout", fields);
+        }
+
+        public static FrameworkElement MakeView(DocumentController documentController, Context context)
+        {
+            //add field binding for content of content presenter
+            ContentPresenter contentPresenter = new ContentPresenter();
+            BindContent(contentPresenter, documentController, context);
+
+            return contentPresenter;
+
+        }
+
+        protected static void BindContent(ContentPresenter presenter, DocumentController docController, Context context)
+        {
+            var converter = new TableFieldToMakeViewConverter(docController, context);
+
+            var contentBinding = new FieldBinding<FieldControllerBase>()
+            {
+                Key = KeyStore.DataKey,
+                Document = docController,
+                Converter = converter,
+                Mode = BindingMode.TwoWay,
+                Context = context,
+                ValueType = BindingValueType.Field
+            };
+            presenter.AddFieldBinding(ContentPresenter.ContentProperty, contentBinding);
+        }
+    }
+}
---
+++ Views/TableFieldToMakeViewConverter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Dash.Controllers;
+using DashShared;
+
+namespace Dash
+{
+    class TableFieldToMakeViewConverter : SafeDataToXamlConverter<FieldControllerBase, FrameworkElement>
+    {
+
+        private DocumentController _docController = null;
+        private Context _context = null;
+        private TypeInfo _lastType = TypeInfo.None;
+        private FrameworkElement _lastElement = null;
+
+        public TableFieldToMakeViewConverter(DocumentController docController, Context context)
+        {
+            _docController = docController;
+            _context = context;
+        }
+
+        public override FrameworkElement ConvertDataToXaml(FieldControllerBase data, object parameter = null)
+        {
+            if (data is TextController txt && txt.Data.StartsWith("=="))
+            {
+                try
+                {
+                    data = DSL.InterpretUserInput(txt.Data)?.DereferenceToRoot(null);
+                }
+                catch (Exception) { }
+            }
+            //if (data is ListController<DocumentController> documentList)
+            //{
+            //    data = new TextController(new ObjectToStringConverter().ConvertDataToXaml(documentList, null));
+            //}
+
+            FrameworkElement currView = null;
+
+            if (_lastType == data?.TypeInfo && _lastType != TypeInfo.Document)
+            {
+                return _lastElement;
+            }
+            if (data is ImageController img)
+            {
+                currView = ImageBox.MakeView(_docController, _context);
+            }
+            if (data is PdfController)
+            {
+                currView = PdfBox.MakeView(_docController, _context);
+            }
+            if (data is VideoController)
+            {
+                currView = VideoBox.MakeView(_docController, _context);
+            }
+            else if (data is AudioController)
+            {
+                currView = AudioBox.MakeView(_docController, _context);
+            }
+            else if (data is ListController<DocumentController> docList)
+            {
+                if (double.IsNaN(_docController.GetWidth()))
+                { // if we're going to show a CollectionBox, give it initial dimensions, otherwise it has no default size
+                    _docController.SetWidth(400);
+                    _docController.SetHeight(400);
+                }
+                currView = CollectionBox.MakeView(_docController, _context);
+            }
+            else if (data is DocumentController dc)
+            {
+                // hack to check if the dc is a view document
+                if (KeyStore.TypeRenderer.ContainsKey(dc.DocumentType))
+                {
+                    currView = dc.MakeViewUI(_context);
+                }
+                else
+                {
+                    currView = dc.GetKeyValueAlias().MakeViewUI(_context);
+                }
+            }
+            else if (data is TextController || data is NumberController || data is DateTimeController)
+            {
+                currView = TextingBox.MakeView(_docController, _context);
+            }
+            else if (data is RichTextController)
+            {
+                currView = RichTextBox.MakeView(_docController, _context);
+            }
+            if (currView == null) currView = new Grid();
+
+            _lastElement = currView;
+            _lastType = data?.TypeInfo ?? TypeInfo.None;
+
+            return currView;
+        }
+
+        public override FieldControllerBase ConvertXamlToData(FrameworkElement xaml, object parameter = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
