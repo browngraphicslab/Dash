be3762231360891a8c3921046ae72f7ba0af3aae
David Wang <david_wang1@brown.edu>
2018-09-29 13:06:03 -0400

    Integrated Phil's stuff

---
+++ Browser/CollapseRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+using Dash.Browser;
+using Dash.Controllers;
+using DashShared;
+
+namespace Dash 
+{
+    public class CollapseRequest : BrowserRequest
+    {
+        public bool expanded { get; set; }
+        public string url { get; set; }
+
+        public override async Task Handle(BrowserView browser)
+        {
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                async () =>
+                {
+                    if (!expanded)
+                    {
+                        var uri = new Uri(url);
+
+                        var layout = await new PdfToDashUtil().UriToDoc(uri);
+                        layout.SetField<BoolController>(KeyStore.AbstractInterfaceKey, true, true);
+
+                        SplitFrame.ActiveFrame.TrySplit(SplitFrame.SplitDirection.Right, layout, true);
+                    }
+                    else
+                    {
+                        var thisPdfDoc = MainPage.Instance.MainSplitter.GetChildFrames().FirstOrDefault(fr =>
+                            fr.DocumentController.GetField<BoolController>(KeyStore.AbstractInterfaceKey) != null);
+                        thisPdfDoc?.Delete();
+                    }
+                });
+        }
+    }
+}
---
+++ ChromeExt2/popup.js
@@ -0,0 +1,25 @@
+﻿document.getElementById("btnSticky").addEventListener("click", function() {
+    chrome.windows.getCurrent(function(e) {
+        chrome.runtime.sendMessage("toggleActive");
+    })
+});
+
+chrome.runtime.onMessage.addListener( function(message) {
+    var button = document.getElementById("btnSticky");
+    var status = document.getElementById("status");
+    if (message == "status:connected") {
+        status.classList.add("active");
+        button.innerText = "Disconnect";
+        status.innerText = "Connected.";
+    } else if (message == "status:disconnected") {
+        status.classList.remove("active");
+        status.innerText = "Disconnected.";
+        button.innerText = "Connect";
+    } else if (message == "status:error") {
+        status.classList.remove("active");
+        status.innerText = "Error: No Service running.";
+        button.innerText = "Connect";
+    }
+});
+
+chrome.runtime.sendMessage("requestStatus");
