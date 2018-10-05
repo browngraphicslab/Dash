62811bf2ab38d328841c83f7c166fd30eb7157fe
hchow <hannah_chow@brown.edu>
2018-10-05 12:55:33 -0400

    Added modes for join operator

--- Controllers/Operators/JoinOperator.cs
+++ Controllers/Operators/JoinOperator.cs
@@ -10,13 +10,13 @@ namespace Dash
     [OperatorType(Op.Name.join)]
     public class JoinOperator : OperatorController
     {
-        public static readonly KeyController SourceTableKey = new KeyController("Tableone");
-        public static readonly KeyController TargetTableKey = new KeyController("Tabletwo");
-        public static readonly KeyController SourceKeyKey = new KeyController("Keyone");
-        public static readonly KeyController TargetKeyKey = new KeyController("Keytwo");
+        public static readonly KeyController SourceTableKey = new KeyController("TableOne");
+        public static readonly KeyController TargetTableKey = new KeyController("TableTwo");
+        public static readonly KeyController SourceKeyKey = new KeyController("KeyOne");
+        public static readonly KeyController TargetKeyKey = new KeyController("KeyTwo");
+        public static readonly KeyController OptionsKey = new KeyController("Options");
 
-
-        public static readonly KeyController GencollectionKey = new KeyController("Gencollection");
+        public static readonly KeyController GencollectionKey = new KeyController("GenCollection");
 
 
         public JoinOperator() : base(new OperatorModel(TypeKey.KeyModel))
@@ -42,6 +42,7 @@ namespace Dash
             new KeyValuePair<KeyController, IOInfo>(TargetTableKey, new IOInfo(DashShared.TypeInfo.Document, true)),
             new KeyValuePair<KeyController, IOInfo>(SourceKeyKey, new IOInfo(DashShared.TypeInfo.Key, true)),
             new KeyValuePair<KeyController, IOInfo>(TargetKeyKey, new IOInfo(DashShared.TypeInfo.Key, true)),
+            new KeyValuePair<KeyController, IOInfo>(TargetKeyKey, new IOInfo(DashShared.TypeInfo.Text, false)),
         };
 
         public override ObservableDictionary<KeyController, DashShared.TypeInfo> Outputs { get; } = new ObservableDictionary<KeyController, DashShared.TypeInfo>
@@ -55,15 +56,20 @@ namespace Dash
             var targetTable = (DocumentController)inputs[TargetTableKey];
             var sourceKey = (KeyController)inputs[SourceKeyKey];
             var targetKey = (KeyController)inputs[TargetKeyKey];
-            var gencollection = Execute(sourceTable, targetTable, sourceKey, targetKey);
+            var optionsMessage = (TextController)inputs[OptionsKey];
+            var gencollection = Execute(sourceTable, targetTable, sourceKey, targetKey, optionsMessage);
             outputs[GencollectionKey] = gencollection;
         }
 
-        public DocumentController Execute(DocumentController sourceTable, DocumentController targetTable, KeyController sourceKey, KeyController targetKey)
+        public DocumentController Execute(DocumentController sourceTable, DocumentController targetTable, KeyController sourceKey, KeyController targetKey, TextController opMess)
         {
-            var generatedCollection = (DocumentController) sourceTable.Copy();
+            // if join mode is unspecified or "New", edit a copy of the source table. 
+            // If set to "Add", will add the fields to matched documents in the original table, and no filtering takes place
+            var addMode = opMess != null && opMess.Data.Equals("Add");
+            var generatedCollection = addMode ? sourceTable : (DocumentController)sourceTable.Copy();
             var sourceDocs = generatedCollection.GetDereferencedField<ListController<DocumentController>>(KeyStore.DataKey, null);
             var targetDocs = targetTable.GetDereferencedField<ListController<DocumentController>>(KeyStore.DataKey, null);
+            var toFilterOut = new List<DocumentController>();
 
             foreach (var row in sourceDocs)
             {
@@ -75,23 +81,30 @@ namespace Dash
                 var valToMatch = dataDoc.GetField(sourceKey).GetValue(null);
                 var matchedDoc = targetDocs.FirstOrDefault(k => k.GetDataDocument().GetField(targetKey).GetValue(null).Equals(valToMatch))?.GetDataDocument();
 
-                if (matchedDoc == null) continue;
+                if (matchedDoc == null)
+                {
+                    if (!addMode) toFilterOut.Add(row);
+                    continue;
+                }
  
-                var targetFields = matchedDoc?.EnumFields().Select(kv => kv.Key).ToList();
+                var targetFields = matchedDoc.EnumFields().Select(kv => kv.Key).ToList();
 
                 foreach (var tarKey in targetFields)
                 {
                     if (sourceFields.Contains(tarKey)) continue;
-                    dataDoc.SetField(tarKey, matchedDoc?.GetField(tarKey), true);
+                    dataDoc.SetField(tarKey, matchedDoc.GetField(tarKey), true);
                 }
 
-                row.SetField(targetKey, matchedDoc?.GetField(targetKey), true);
-                dataDoc.SetField(targetKey, matchedDoc?.GetField(targetKey), true);
+                row.SetField(targetKey, matchedDoc.GetField(targetKey), true);
+                dataDoc.SetField(targetKey, matchedDoc.GetField(targetKey), true);
             }
+            toFilterOut.ForEach(doc => generatedCollection.RemoveFromListField(KeyStore.DataKey, doc));
 
-
-            generatedCollection.SetField<TextController>(KeyStore.TitleKey, $"Generated by joining on {sourceKey} and {targetKey}", true);
-            //MainPage.Instance.ViewModel.AddDocument(generatedCollection);
+            var sourceTitle = sourceTable.GetDereferencedField(KeyStore.TitleKey, null);
+            var targetTitle = targetTable.GetDereferencedField(KeyStore.TitleKey, null);
+            var joinMessage = $"Generated by joining on {sourceKey} in '{sourceTitle}' and {targetKey} in '{targetTitle}'";
+            generatedCollection.SetField<TextController>(KeyStore.JoinInfoKey, joinMessage, true);
+            generatedCollection.SetField<TextController>(KeyStore.TitleKey, $"{sourceTitle} (Joined)", true);
 
             return generatedCollection;
         }
