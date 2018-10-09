ae8883b4c07fc4fe990aae7f01c73cf31b6e9fc5
madelinegr <mgriswold99@gmail.com>
2018-10-09 01:28:41 -0400

    guhkihfkehjgkjhekrhjtg

--- Views/KVPRow.xaml.cs
+++ Views/KVPRow.xaml.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -19,9 +24,27 @@ using Dash;
 namespace Dash
 {
 
-    public sealed partial class KVPRow : UserControl
+    public sealed partial class KVPRow : UserControl, INotifyPropertyChanged
     {
         public EditableScriptViewModel ViewModel => DataContext as EditableScriptViewModel;
+        public bool IsEditing
+        {
+            get => _isEditing;
+            set
+            {
+                if (value == _isEditing) return;
+                _isEditing = value;
+                OnPropertyChanged();
+                xEditBox.Focus(FocusState.Programmatic);
+            }
+        }
+
+        private bool _isEditing;
+
+        private Visibility Not(bool b)
+        {
+            return b ? Visibility.Collapsed : Visibility.Visible;
+        }
 
         public KVPRow()
         {
@@ -49,6 +72,75 @@ namespace Dash
             var key = ViewModel.Key;
 
             TableBox.BindContent(XValuePresenter, document, key, null);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Delete_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Edit_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            IsEditing = true;
+
+            if (xEditBox.Height > 50)
+            {
+                xEditBox.Height = XValuePresenter.ActualHeight;
+            }
+            else
+            {
+                xEditBox.Height = 50;
+            }
+            
+        }
+
+        private void XEditBox_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            xEditBox.Text = DSL.GetScriptForField(ViewModel.Document.GetField(ViewModel.Key));
+            xEditBox.SelectAll();
+        }
+
+        private void XEditBox_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            CancelEdit();
+        }
+
+        private async void XEditBox_OnKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                await CommitEdit();
+            }
+            else if (e.Key == VirtualKey.Escape)
+            {
+                CancelEdit();
+            }
+        }
+
+        private async Task CommitEdit()
+        {
+            using (UndoManager.GetBatchHandle())
+            {
+                var field = await DSL.InterpretUserInput(xEditBox.Text);
+                if (field != null)
+                {
+                    ViewModel.Document.SetField(ViewModel.Key, field, true);
+                }
+
+                IsEditing = false;
+            }
+        }
+
+        private void CancelEdit()
+        {
+            IsEditing = false;
         }
     }
 }
