ef316926e192e41c93287deaaf3234406685301d
Madeline <mgriswold99@gmail.com>
2018-11-10 15:56:51 -0500

    voice commands

---
+++ Util/VoiceCommandService.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.VoiceCommands;
+
+namespace Dash
+{
+    public sealed class VoiceCommandService : IBackgroundTask
+    {
+        private BackgroundTaskDeferral serviceDeferral;
+        VoiceCommandServiceConnection voiceServiceConnection;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            //Take a service deferral so the service isn&#39;t terminated.
+            this.serviceDeferral = taskInstance.GetDeferral();
+
+            //TODO: task canclled
+            //taskInstance.Canceled += OnTaskCanceled;
+
+            var triggerDetails = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+
+            if (triggerDetails != null  && /*& amp; &amp;*/
+            triggerDetails.Name == "AdventureWorksVoiceServiceEndpoint") {
+                try
+                {
+                    voiceServiceConnection =
+                    VoiceCommandServiceConnection.FromAppServiceTriggerDetails(
+                        triggerDetails);
+                    voiceServiceConnection.VoiceCommandCompleted +=
+                    VoiceCommandCompleted;
+
+                    VoiceCommand voiceCommand = await
+                    voiceServiceConnection.GetVoiceCommandAsync();
+
+                    switch (voiceCommand.CommandName)
+                    {
+                    case "whenIsTripToDestination":
+                        {
+                            var destination =
+                            voiceCommand.Properties["destination"][0];
+                            SendCompletionMessageForDestination(destination);
+                            break;
+                        }
+
+                    // As a last resort, launch the app in the foreground.
+                    default:
+                        LaunchAppInForeground();
+                        break;
+                    }
+                }
+                finally
+                {
+                    if (this.serviceDeferral != null)
+                    {
+                        // Complete the service deferral.
+                        this.serviceDeferral.Complete();
+                    }
+                }
+            }
+        }
+
+        private void VoiceCommandCompleted(VoiceCommandServiceConnection sender,
+            VoiceCommandCompletedEventArgs args)
+        {
+            if (this.serviceDeferral != null)
+            {
+                // Insert your code here.
+                // Complete the service deferral.
+                this.serviceDeferral.Complete();
+            }
+        }
+
+        private async void SendCompletionMessageForDestination(
+            string destination)
+        {
+            // Take action and determine when the next trip to destination
+            // Insert code here.
+
+            // Replace the hardcoded strings used here with strings 
+            // appropriate for your application.
+
+            // First, create the VoiceCommandUserMessage with the strings 
+            // that Cortana will show and speak.
+            var userMessage = new VoiceCommandUserMessage();
+            userMessage.DisplayMessage = "Here's your trip.";
+            userMessage.SpokenMessage = "Your trip to Vegas is on August 3rd.";
+
+            // Optionally, present visual information about the answer.
+            // For this example, create a VoiceCommandContentTile with an 
+            // icon and a string.
+            var destinationsContentTiles = new List<VoiceCommandContentTile>();
+
+            var destinationTile = new VoiceCommandContentTile();
+            destinationTile.ContentTileType =
+                VoiceCommandContentTileType.TitleWith68x68IconAndText;
+            // The user taps on the visual content to launch the app. 
+            // Pass in a launch argument to enable the app to deep link to a 
+            // page relevant to the item displayed on the content tile.
+            destinationTile.AppLaunchArgument =
+                string.Format("destination={0}", "Las Vegas");
+            destinationTile.Title = "Las Vegas";
+            destinationTile.TextLine1 = "August 3rd 2015";
+            destinationsContentTiles.Add(destinationTile);
+
+            // Create the VoiceCommandResponse from the userMessage and list    
+            // of content tiles.
+            var response = VoiceCommandResponse.CreateResponse(
+                userMessage, destinationsContentTiles);
+
+            // Cortana displays a "Go to app_name" link that the user 
+            // taps to launch the app. 
+            // Pass in a launch to enable the app to deep link to a page 
+            // relevant to the voice command.
+            response.AppLaunchArgument = string.Format(
+                "destination={0}", "Las Vegas");
+
+            // Ask Cortana to display the user message and content tile and 
+            // also speak the user message.
+            await voiceServiceConnection.ReportSuccessAsync(response);
+        }
+
+        private async void LaunchAppInForeground()
+        {
+            var userMessage = new VoiceCommandUserMessage();
+            userMessage.SpokenMessage = "Launching Adventure Works";
+
+            var response = VoiceCommandResponse.CreateResponse(userMessage);
+
+            // When launching the app in the foreground, pass an app 
+            // specific launch parameter to indicate what page to show.
+            response.AppLaunchArgument = "showAllTrips=true";
+
+            await voiceServiceConnection.RequestAppLaunchAsync(response);
+        }
+    }
+}
