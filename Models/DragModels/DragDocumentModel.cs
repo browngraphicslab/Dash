90d0a6cfc0441c89ba9a3a80fefd8bf1c4b75abb
bob <bcz@cs.brown.edu>
2018-11-27 12:01:20 -0500

    cleaned up travelog xaml a bit.

--- Models/DragModels/DragDocumentModel.cs
+++ Models/DragModels/DragDocumentModel.cs
@@ -194,18 +194,23 @@ namespace Dash
                             xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
                             xmlns:dash=""using:Dash""
                             xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006"">
-                            <Grid.RowDefinitions>
-                                <RowDefinition Height=""Auto""></RowDefinition>
-                                <RowDefinition Height=""*""></RowDefinition>
-                            </Grid.RowDefinitions>
-                            <Border BorderThickness=""2"" BorderBrush=""CadetBlue"" Background=""White"">
-                                <TextBlock x:Name=""xTextFieldData"" HorizontalAlignment=""Stretch"" Height=""Auto"" VerticalAlignment=""Top""/>
-                            </Border>
-                            <StackPanel Orientation=""Horizontal"" Grid.Row=""1"" Background=""Transparent"">
-                                <dash:DocumentView x:Name=""xDocumentField_EventDisplay1Key"" HorizontalAlignment=""Stretch"" VerticalAlignment=""Center"" />
-                                <TextBlock FontFamily=""{StaticResource FontAwesome}"" VerticalAlignment=""Center"" FontSize=""20"" Foreground=""Blue"" Text=""{StaticResource RightArrowIcon}""/>
-                                <dash:DocumentView x:Name=""xDocumentField_EventDisplay2Key"" HorizontalAlignment=""Stretch"" VerticalAlignment=""Center"" />
-                            </StackPanel>
+                                <Grid.RowDefinitions>
+                                    <RowDefinition Height=""Auto""></RowDefinition>
+                                    <RowDefinition Height=""*""></RowDefinition>
+                                </Grid.RowDefinitions>
+                                <Border BorderThickness=""2"" BorderBrush=""CadetBlue"" Background=""White"">
+                                    <TextBlock x:Name=""xTextFieldData"" HorizontalAlignment=""Stretch"" Height=""Auto"" VerticalAlignment=""Top""/>
+                                </Border>
+                                <Grid Grid.Row=""1"" Background=""Transparent"">
+                                    <Grid.ColumnDefinitions>
+                                        <ColumnDefinition Width=""*""></ColumnDefinition>
+                                        <ColumnDefinition Width=""25""></ColumnDefinition>
+                                        <ColumnDefinition Width=""*""></ColumnDefinition>
+                                    </Grid.ColumnDefinitions>
+                                    <dash:DocumentView x:Name=""xDocumentField_EventDisplay1Key"" HorizontalAlignment=""Stretch"" VerticalAlignment=""Center"" Grid.Column=""0"" />
+                                    <TextBlock FontFamily=""{StaticResource FontAwesome}"" VerticalAlignment=""Center"" FontSize=""20"" Foreground=""Blue"" Grid.Column=""1"" Text=""{StaticResource RightArrowIcon}""/>
+                                    <dash:DocumentView x:Name=""xDocumentField_EventDisplay2Key"" HorizontalAlignment=""Stretch"" VerticalAlignment=""Center"" Grid.Column=""2"" />
+                                </Grid>
                             </Grid>";
                         EventManager.EventOccured(eventDoc, displayXaml);
                     }
