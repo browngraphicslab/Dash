c140a16b115e8145127722ca34879566598f0fca
Brandon <brandon_li@brown.edu>
2018-11-27 20:55:32 -0500

    started content presenter flashcard stuff

---
+++ Controllers/Functions/Operators/TemplateFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dash
+{
+    public class TemplateFunctions
+    {
+        public static TextController ContentTemplate(TextController keyName)
+        {
+            //TODO: return new textcontroller with Xaml
+        }
+
+        public static TextController TextTemplate(TextController keyName)
+        {
+        }
+
+        public static TextController ImageTemplate(TextController keyName)
+        {
+        }
+    }
+}
